7bc27fac82840ceb07b1afc76254b46840efedfe
Eren Golge <egolge@mozilla.com>
2018-04-25 08:02:56 -0700

    Remove variables

--- tests/layers_tests.py
+++ tests/layers_tests.py
@@ -9,7 +9,7 @@ class PrenetTests(unittest.TestCase):
 
     def test_in_out(self):
         layer = Prenet(128, out_features=[256, 128])
-        dummy_input = T.autograd.Variable(T.rand(4, 128))
+        dummy_input = T.rand(4, 128)
 
         print(layer)
         output = layer(dummy_input)
@@ -21,7 +21,7 @@ class CBHGTests(unittest.TestCase):
 
     def test_in_out(self):
         layer = CBHG(128, K=6, projections=[128, 128], num_highways=2)
-        dummy_input = T.autograd.Variable(T.rand(4, 8, 128))
+        dummy_input = T.rand(4, 8, 128)
 
         print(layer)
         output = layer(dummy_input)
@@ -34,8 +34,8 @@ class DecoderTests(unittest.TestCase):
 
     def test_in_out(self):
         layer = Decoder(in_features=256, memory_dim=80, r=2)
-        dummy_input = T.autograd.Variable(T.rand(4, 8, 256))
-        dummy_memory = T.autograd.Variable(T.rand(4, 2, 80))
+        dummy_input = T.rand(4, 8, 256)
+        dummy_memory = T.rand(4, 2, 80)
 
         output, alignment = layer(dummy_input, dummy_memory)
 
@@ -48,7 +48,7 @@ class EncoderTests(unittest.TestCase):
 
     def test_in_out(self):
         layer = Encoder(128)
-        dummy_input = T.autograd.Variable(T.rand(4, 8, 128))
+        dummy_input = T.rand(4, 8, 128)
 
         print(layer)
         output = layer(dummy_input)
@@ -62,24 +62,22 @@ class L1LossMaskedTests(unittest.TestCase):
 
     def test_in_out(self):
         layer = L1LossMasked()
-        dummy_input = T.autograd.Variable(T.ones(4, 8, 128).float())
-        dummy_target = T.autograd.Variable(T.ones(4, 8, 128).float())
-        dummy_length = T.autograd.Variable((T.ones(4) * 8).long())
+        dummy_input = T.ones(4, 8, 128).float()
+        dummy_target = T.ones(4, 8, 128).float()
+        dummy_length = (T.ones(4) * 8).long()
         output = layer(dummy_input, dummy_target, dummy_length)
-        assert output.shape[0] == 0
-        assert len(output.shape) == 1
-        assert output.data[0] == 0.0
+        assert output.item() == 0.0
 
-        dummy_input = T.autograd.Variable(T.ones(4, 8, 128).float())
-        dummy_target = T.autograd.Variable(T.zeros(4, 8, 128).float())
-        dummy_length = T.autograd.Variable((T.ones(4) * 8).long())
+        dummy_input = T.ones(4, 8, 128).float()
+        dummy_target = T.zeros(4, 8, 128).float()
+        dummy_length = (T.ones(4) * 8).long()
         output = layer(dummy_input, dummy_target, dummy_length)
-        assert output.data[0] == 1.0, "1.0 vs {}".format(output.data[0])
+        assert output.item() == 1.0, "1.0 vs {}".format(output.data[0])
 
-        dummy_input = T.autograd.Variable(T.ones(4, 8, 128).float())
-        dummy_target = T.autograd.Variable(T.zeros(4, 8, 128).float())
-        dummy_length = T.autograd.Variable((T.arange(5, 9)).long())
+        dummy_input = T.ones(4, 8, 128).float()
+        dummy_target = T.zeros(4, 8, 128).float()
+        dummy_length = (T.arange(5, 9)).long()
         mask = ((_sequence_mask(dummy_length).float() - 1.0)
                 * 100.0).unsqueeze(2)
         output = layer(dummy_input + mask, dummy_target, dummy_length)
-        assert output.data[0] == 1.0, "1.0 vs {}".format(output.data[0])
+        assert output.item() == 1.0, "1.0 vs {}".format(output.data[0])
