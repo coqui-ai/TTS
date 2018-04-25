07f71b1761e4f328acc3abe29b676c2e3bae42d8
Eren Golge <egolge@mozilla.com>
2018-04-25 08:00:48 -0700

    Remove variables

--- layers/tacotron.py
+++ layers/tacotron.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import torch
-from torch.autograd import Variable
 from torch import nn
 
 from .attention import AttentionRNN
@@ -263,16 +262,16 @@ class Decoder(nn.Module):
                                                               self.memory_dim, self.r)
             T_decoder = memory.size(1)
         # go frame - 0 frames tarting the sequence
-        initial_memory = Variable(
-            inputs.data.new(B, self.memory_dim * self.r).zero_())
+        initial_memory = \
+            inputs.data.new(B, self.memory_dim * self.r).zero_().requires_grad_()
         # Init decoder states
-        attention_rnn_hidden = Variable(
-            inputs.data.new(B, 256).zero_())
-        decoder_rnn_hiddens = [Variable(
-            inputs.data.new(B, 256).zero_())
+        attention_rnn_hidden = \
+            inputs.data.new(B, 256).zero_().requires_grad_()
+        decoder_rnn_hiddens = [
+            inputs.data.new(B, 256).zero_().requires_grad_()
             for _ in range(len(self.decoder_rnns))]
-        current_context_vec = Variable(
-            inputs.data.new(B, 256).zero_())
+        current_context_vec = \
+            inputs.data.new(B, 256).zero_().requires_grad_()
         # Time first (T_decoder, B, memory_dim)
         if memory is not None:
             memory = memory.transpose(0, 1)
