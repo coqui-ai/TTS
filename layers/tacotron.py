67dfb2f01ddd8362b8e42e90b75e2536544f1f02
Eren Golge <egolge@mozilla.com>
2018-04-29 06:12:14 -0700

    thweb finetune

--- layers/tacotron.py
+++ layers/tacotron.py
@@ -324,4 +324,4 @@ class Decoder(nn.Module):
 
 def is_end_of_frames(output, alignment, eps=0.01):  # 0.2
     return ((output.data <= eps).prod(0) > 0).any() \
-            and alignment.data[:, alignment.shape[1] - int(alignment.shape[1]/9):].sum() > 0.7
+            and alignment.data[:, -10:].sum() > 0.7
