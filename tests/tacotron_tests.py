b8885fe4b6f2523e273ad0b45ab074f68bb71386
Eren Golge <egolge@mozilla.com>
2018-04-30 06:01:02 -0700

    add stop token to tacotron testing

--- tests/tacotron_tests.py
+++ tests/tacotron_tests.py
@@ -5,6 +5,7 @@ import unittest
 import numpy as np
 
 from torch import optim
+from torch import nn
 from TTS.utils.generic_utils import load_config
 from TTS.layers.losses import L1LossMasked
 from TTS.models.tacotron import Tacotron
@@ -24,7 +25,11 @@ class TacotronTrainTest(unittest.TestCase):
         mel_spec = torch.rand(8, 30, c.num_mels).to(device)
         linear_spec = torch.rand(8, 30, c.num_freq).to(device)
         mel_lengths = torch.randint(20, 30, (8,)).long().to(device)
+        stop_targets = torch.zeros(8, 30, 1).float().to(device)
+        for idx in mel_lengths:
+            stop_targets[:, int(idx.item()):, 0] = 1.0
         criterion = L1LossMasked().to(device)
+        criterion_st = nn.BCELoss().to(device)
         model = Tacotron(c.embedding_size,
                          c.num_freq,
                          c.num_mels,
@@ -37,17 +42,20 @@ class TacotronTrainTest(unittest.TestCase):
             count += 1
         optimizer = optim.Adam(model.parameters(), lr=c.lr)
         for i in range(5):
-            mel_out, linear_out, align = model.forward(input, mel_spec)
+            mel_out, linear_out, align, stop_tokens = model.forward(input, mel_spec)
+            assert stop_tokens.data.max() <= 1.0
+            assert stop_tokens.data.min() >= 0.0
             optimizer.zero_grad()
             loss = criterion(mel_out, mel_spec, mel_lengths) 
-            loss = 0.5 * loss + 0.5 * criterion(linear_out, linear_spec, mel_lengths)
+            stop_loss = criterion_st(stop_tokens, stop_targets)
+            loss = loss + criterion(linear_out, linear_spec, mel_lengths) + stop_loss
             loss.backward()
             optimizer.step()
         # check parameter changes
         count = 0
         for param, param_ref in zip(model.parameters(), model_ref.parameters()):
             # ignore pre-higway layer since it works conditional 
-            if count not in [139, 59]:
+            if count not in [141, 59]:
                 assert (param != param_ref).any(), "param {} with shape {} not updated!! \n{}\n{}".format(count, param.shape, param, param_ref)
             count += 1
             
