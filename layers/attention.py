2bdabc047ad2946737a29051fc74a9b02dad9996
Eren Golge <egolge@mozilla.com>
2018-05-25 03:24:45 -0700

    Remove empty lines

--- layers/attention.py
+++ layers/attention.py
@@ -22,11 +22,9 @@ class BahdanauAttention(nn.Module):
         # (batch, 1, dim)
         processed_query = self.query_layer(query)
         processed_annots = self.annot_layer(annots)
-
         # (batch, max_time, 1)
         alignment = self.v(nn.functional.tanh(
             processed_query + processed_annots))
-
         # (batch, max_time)
         return alignment.squeeze(-1)
 
@@ -54,31 +52,23 @@ class AttentionRNN(nn.Module):
 
     def forward(self, memory, context, rnn_state, annotations,
                 mask=None, annotations_lengths=None):
-
         if annotations_lengths is not None and mask is None:
             mask = get_mask_from_lengths(annotations, annotations_lengths)
-
         # Concat input query and previous context context
         rnn_input = torch.cat((memory, context), -1)
-        #rnn_input = rnn_input.unsqueeze(1)
-
         # Feed it to RNN
         # s_i = f(y_{i-1}, c_{i}, s_{i-1})
         rnn_output = self.rnn_cell(rnn_input, rnn_state)
-
         # Alignment
         # (batch, max_time)
         # e_{ij} = a(s_{i-1}, h_j)
         alignment = self.alignment_model(annotations, rnn_output)
-
         # TODO: needs recheck.
         if mask is not None:
             mask = mask.view(query.size(0), -1)
             alignment.data.masked_fill_(mask, self.score_mask_value)
-
         # Normalize context weight
         alignment = F.softmax(alignment, dim=-1)
-
         # Attention context vector
         # (batch, 1, dim)
         # c_i = \sum_{j=1}^{T_x} \alpha_{ij} h_j
