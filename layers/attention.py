7e020d40845bd662823393afe2f9eda53c8d953b
Eren Golge <egolge@mozilla.com>
2019-01-16 16:23:04 +0100

    Bug fixes

--- layers/attention.py
+++ layers/attention.py
@@ -170,9 +170,9 @@ class AttentionRNNCell(nn.Module):
         # Update the window
         self.win_idx = torch.argmax(alignment,1).long()[0].item()
         # Normalize context weight
-        # alignment = F.softmax(alignment, dim=-1)
+        alignment = F.softmax(alignment, dim=-1)
         # alignment = 5 * alignment
-        alignment = torch.sigmoid(alignment) / torch.sigmoid(alignment).sum(dim=1).unsqueeze(1)
+        # alignment = torch.sigmoid(alignment) / torch.sigmoid(alignment).sum(dim=1).unsqueeze(1)
         # Attention context vector
         # (batch, 1, dim)
         # c_i = \sum_{j=1}^{T_x} \alpha_{ij} h_j
