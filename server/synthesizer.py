95de2cd5595a403bdd59565bcf6dc4b80b00dfa5
gnosly <fabriziogiovannetti@gmail.com>
2019-03-11 21:56:40 +0100

    added missing phonemes, synthesizer.py now setup the correct input layer

--- server/synthesizer.py
+++ server/synthesizer.py
@@ -1,16 +1,13 @@
 import io
 import os
-import librosa
-import torch
-import scipy
-import numpy as np
-import soundfile as sf
-from utils.text import text_to_sequence
-from utils.generic_utils import load_config
-from utils.audio import AudioProcessor
-from models.tacotron import Tacotron
-from matplotlib import pylab as plt
 
+import numpy as np
+import torch
+
+from models.tacotron import Tacotron
+from utils.audio import AudioProcessor
+from utils.generic_utils import load_config
+from utils.text import phoneme_to_sequence, phonemes, symbols, text_to_sequence
 
 class Synthesizer(object):
     def load_model(self, model_path, model_name, model_config, use_cuda):
@@ -22,14 +19,22 @@ class Synthesizer(object):
         config = load_config(model_config)
         self.config = config
         self.use_cuda = use_cuda
+        self.use_phonemes = config.use_phonemes
         self.ap = AudioProcessor(**config.audio)
-        self.model = Tacotron(config.embedding_size, self.ap.num_freq, self.ap.num_mels, config.r)
+
+        if self.use_phonemes:
+            self.input_size = len(phonemes)
+            self.input_adapter = lambda sen: phoneme_to_sequence(sen, [self.config.text_cleaner], self.config.phoneme_language)
+        else:
+            self.input_size = len(symbols)
+            self.input_adapter = lambda sen: text_to_sequence(sen, [self.config.text_cleaner])
+
+        self.model = Tacotron(self.input_size, config.embedding_size, self.ap.num_freq, self.ap.num_mels, config.r)
         # load model state
         if use_cuda:
             cp = torch.load(self.model_file)
         else:
-            cp = torch.load(
-                self.model_file, map_location=lambda storage, loc: storage)
+            cp = torch.load(self.model_file, map_location=lambda storage, loc: storage)
         # load the model
         self.model.load_state_dict(cp['model'])
         if use_cuda:
@@ -42,7 +47,6 @@ class Synthesizer(object):
         self.ap.save_wav(wav, path)
 
     def tts(self, text):
-        text_cleaner = [self.config.text_cleaner]
         wavs = []
         for sen in text.split('.'):
             if len(sen) < 3:
@@ -51,7 +55,9 @@ class Synthesizer(object):
             sen += '.'
             print(sen)
             sen = sen.strip()
-            seq = np.array(text_to_sequence(sen, text_cleaner))
+
+            seq = np.array(self.input_adapter(sen))
+
             chars_var = torch.from_numpy(seq).unsqueeze(0).long()
             if self.use_cuda:
                 chars_var = chars_var.cuda()
@@ -59,8 +65,9 @@ class Synthesizer(object):
                 chars_var)
             linear_out = linear_out[0].data.cpu().numpy()
             wav = self.ap.inv_spectrogram(linear_out.T)
-            out = io.BytesIO()
             wavs += list(wav)
             wavs += [0] * 10000
+
+        out = io.BytesIO()
         self.save_wav(wavs, out)
         return out
