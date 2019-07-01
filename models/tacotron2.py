482eced0e990a9bebb761a9c6185e8eadbc69a79
Thomas Werkmeister <thomas@twerkmeister.com>
2019-07-01 14:00:44 +0200

    disabling multispeaker with num_speakers=0

--- models/tacotron2.py
+++ models/tacotron2.py
@@ -29,8 +29,9 @@ class Tacotron2(nn.Module):
         std = sqrt(2.0 / (num_chars + 512))
         val = sqrt(3.0) * std  # uniform bounds for std
         self.embedding.weight.data.uniform_(-val, val)
-        self.speaker_embedding = nn.Embedding(num_speakers, 512)
-        self.speaker_embedding.weight.data.normal_(0, 0.3)
+        if num_speakers > 0:
+            self.speaker_embedding = nn.Embedding(num_speakers, 512)
+            self.speaker_embedding.weight.data.normal_(0, 0.3)
         self.encoder = Encoder(512)
         self.decoder = Decoder(512, self.n_mel_channels, r, attn_win,
                                attn_norm, prenet_type, prenet_dropout,
@@ -43,19 +44,13 @@ class Tacotron2(nn.Module):
         mel_outputs_postnet = mel_outputs_postnet.transpose(1, 2)
         return mel_outputs, mel_outputs_postnet, alignments
 
-    def forward(self, text, speaker_ids, text_lengths, mel_specs=None):
+    def forward(self, text, text_lengths, mel_specs=None, speaker_ids=None):
         # compute mask for padding
         mask = sequence_mask(text_lengths).to(text.device)
         embedded_inputs = self.embedding(text).transpose(1, 2)
         encoder_outputs = self.encoder(embedded_inputs, text_lengths)
-        speaker_embeddings = self.speaker_embedding(speaker_ids)
-
-        speaker_embeddings.unsqueeze_(1)
-        speaker_embeddings = speaker_embeddings.expand(encoder_outputs.size(0),
-                                                       encoder_outputs.size(1),
-                                                       -1)
-
-        encoder_outputs += speaker_embeddings
+        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
+                                                      speaker_ids)
         mel_outputs, stop_tokens, alignments = self.decoder(
             encoder_outputs, mel_specs, mask)
         mel_outputs_postnet = self.postnet(mel_outputs)
@@ -64,16 +59,11 @@ class Tacotron2(nn.Module):
             mel_outputs, mel_outputs_postnet, alignments)
         return mel_outputs, mel_outputs_postnet, alignments, stop_tokens
 
-    def inference(self, text, speaker_ids):
+    def inference(self, text, speaker_ids=None):
         embedded_inputs = self.embedding(text).transpose(1, 2)
         encoder_outputs = self.encoder.inference(embedded_inputs)
-        speaker_embeddings = self.speaker_embedding(speaker_ids)
-
-        speaker_embeddings.unsqueeze_(1)
-        speaker_embeddings = speaker_embeddings.expand(encoder_outputs.size(0),
-                                                       encoder_outputs.size(1),
-                                                       -1)
-        encoder_outputs += speaker_embeddings
+        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
+                                                      speaker_ids)
         mel_outputs, stop_tokens, alignments = self.decoder.inference(
             encoder_outputs)
         mel_outputs_postnet = self.postnet(mel_outputs)
@@ -82,19 +72,14 @@ class Tacotron2(nn.Module):
             mel_outputs, mel_outputs_postnet, alignments)
         return mel_outputs, mel_outputs_postnet, alignments, stop_tokens
 
-    def inference_truncated(self, text, speaker_ids):
+    def inference_truncated(self, text, speaker_ids=None):
         """
         Preserve model states for continuous inference
         """
         embedded_inputs = self.embedding(text).transpose(1, 2)
         encoder_outputs = self.encoder.inference_truncated(embedded_inputs)
-        speaker_embeddings = self.speaker_embedding(speaker_ids)
-
-        speaker_embeddings.unsqueeze_(1)
-        speaker_embeddings = speaker_embeddings.expand(encoder_outputs.size(0),
-                                                       encoder_outputs.size(1),
-                                                       -1)
-        encoder_outputs += speaker_embeddings
+        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
+                                                      speaker_ids)
         mel_outputs, stop_tokens, alignments = self.decoder.inference_truncated(
             encoder_outputs)
         mel_outputs_postnet = self.postnet(mel_outputs)
@@ -102,3 +87,14 @@ class Tacotron2(nn.Module):
         mel_outputs, mel_outputs_postnet, alignments = self.shape_outputs(
             mel_outputs, mel_outputs_postnet, alignments)
         return mel_outputs, mel_outputs_postnet, alignments, stop_tokens
+
+    def _add_speaker_embedding(self, encoder_outputs, speaker_ids):
+        if hasattr(self, "speaker_embedding") and speaker_ids is not None:
+            speaker_embeddings = self.speaker_embedding(speaker_ids)
+
+            speaker_embeddings.unsqueeze_(1)
+            speaker_embeddings = speaker_embeddings.expand(encoder_outputs.size(0),
+                                                           encoder_outputs.size(1),
+                                                           -1)
+            encoder_outputs += speaker_embeddings
+        return encoder_outputs
