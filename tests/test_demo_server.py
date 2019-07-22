fffb5446d8f58d142001dbdf1c007ee1005ae791
Eren Golge <egolge@mozilla.com>
2019-07-22 15:46:26 +0200

    add server Synthesizer test

---
+++ tests/test_demo_server.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+import torch as T
+
+from server.synthesizer import Synthesizer
+from tests import get_tests_input_path, get_tests_output_path, get_tests_path
+from utils.text.symbols import phonemes, symbols
+from utils.generic_utils import load_config, save_checkpoint, setup_model
+
+
+class DemoServerTest(unittest.TestCase):
+    def _create_random_model(self):
+        config = load_config('config.json')
+        num_chars = len(phonemes) if config.use_phonemes else len(symbols)
+        model = setup_model(num_chars, 0, config)
+        output_path = os.path.join(get_tests_output_path())
+        save_checkpoint(model, None, None, None, output_path, 10, 10)
+
+    def test_in_out(self):
+        self._create_random_model()
+        config = load_config(os.path.join(get_tests_input_path(), 'server_config.json'))
+        synthesizer = Synthesizer(config)
+        synthesizer.tts("Better this test works!!")
