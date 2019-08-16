91710503a8234fa08eafc3de0fa7cec64c82ba23
Eren Golge <egolge@mozilla.com>
2019-08-16 15:08:04 +0200

    bug fixes

--- tests/symbols_tests.py
+++ tests/symbols_tests.py
@@ -1,7 +1,9 @@
 import unittest
 
 from utils.text import phonemes
+from collections import Counter
 
 class SymbolsTest(unittest.TestCase):
     def test_uniqueness(self):
-        assert sorted(phonemes) == sorted(list(set(phonemes)))
+        assert sorted(phonemes) == sorted(list(set(phonemes))), " {} vs {} ".format(len(phonemes), len(set(phonemes)))
+        
