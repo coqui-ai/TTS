14a4d1a061872622d992ec39369bd24a78e292cc
Eren Golge <egolge@mozilla.com>
2019-09-12 23:06:59 +0200

    update TacotronGST and its test. Inherit it from Tacotron class

--- models/tacotrongst.py
+++ models/tacotrongst.py
@@ -1,11 +1,13 @@
 # coding: utf-8
+import torch
 from torch import nn
 from TTS.layers.tacotron import Encoder, Decoder, PostCBHG
 from TTS.layers.gst_layers import GST
 from TTS.utils.generic_utils import sequence_mask
+from TTS.models.tacotron import Tacotron
 
 
-class TacotronGST(nn.Module):
+class TacotronGST(Tacotron):
     def __init__(self,
                  num_chars,
                  num_speakers,
@@ -22,37 +24,49 @@ class TacotronGST(nn.Module):
                  forward_attn_mask=False,
                  location_attn=True,
                  separate_stopnet=True):
-        super(TacotronGST, self).__init__()
-        self.r = r
-        self.mel_dim = mel_dim
-        self.linear_dim = linear_dim
-        self.embedding = nn.Embedding(num_chars, 256)
-        self.embedding.weight.data.normal_(0, 0.3)
-        if num_speakers > 1:
-            self.speaker_embedding = nn.Embedding(num_speakers, 256)
-            self.speaker_embedding.weight.data.normal_(0, 0.3)
-        self.encoder = Encoder(256)
-        self.gst = GST(num_mel=80, num_heads=4, num_style_tokens=10, embedding_dim=256)
-        self.decoder = Decoder(256, mel_dim, r, memory_size, attn_win,
+        super().__init__(num_chars,
+                        num_speakers,
+                        r,
+                        linear_dim,
+                        mel_dim,
+                        memory_size,
+                        attn_win,
+                        attn_norm,
+                        prenet_type,
+                        prenet_dropout,
+                        forward_attn,
+                        trans_agent,
+                        forward_attn_mask,
+                        location_attn,
+                        separate_stopnet)
+        gst_embedding_dim = 256
+        decoder_dim = 512 + gst_embedding_dim if num_speakers > 1 else 256 + gst_embedding_dim
+        proj_speaker_dim = 80 if num_speakers > 1 else 0
+        self.decoder = Decoder(decoder_dim, mel_dim, r, memory_size, attn_win,
                                attn_norm, prenet_type, prenet_dropout,
                                forward_attn, trans_agent, forward_attn_mask,
-                               location_attn, separate_stopnet)
-        self.postnet = PostCBHG(mel_dim)
-        self.last_linear = nn.Linear(self.postnet.cbhg.gru_features * 2, linear_dim)
-        
+                               location_attn, separate_stopnet, proj_speaker_dim)
+        self.gst = GST(num_mel=80, num_heads=4,
+                       num_style_tokens=10, embedding_dim=gst_embedding_dim)
 
     def forward(self, characters, text_lengths, mel_specs, speaker_ids=None):
         B = characters.size(0)
         mask = sequence_mask(text_lengths).to(characters.device)
         inputs = self.embedding(characters)
+        self._init_states()
+        self.compute_speaker_embedding(speaker_ids)
+        if self.num_speakers > 1:
+            inputs = self._concat_speaker_embedding(inputs,
+                                                self.speaker_embeddings)
         encoder_outputs = self.encoder(inputs)
-        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
-                                                      speaker_ids)
+        if self.num_speakers > 1:
+            encoder_outputs = self._concat_speaker_embedding(encoder_outputs,
+                                                        self.speaker_embeddings)
         gst_outputs = self.gst(mel_specs)
-        gst_outputs = gst_outputs.expand(-1, encoder_outputs.size(1), -1)
-        encoder_outputs = encoder_outputs + gst_outputs
+        encoder_outputs = self._concat_speaker_embedding(
+            encoder_outputs, gst_outputs)
         mel_outputs, alignments, stop_tokens = self.decoder(
-            encoder_outputs, mel_specs, mask)
+            encoder_outputs, mel_specs, mask, self.speaker_embeddings_projected)
         mel_outputs = mel_outputs.view(B, -1, self.mel_dim)
         linear_outputs = self.postnet(mel_outputs)
         linear_outputs = self.last_linear(linear_outputs)
@@ -61,27 +75,23 @@ class TacotronGST(nn.Module):
     def inference(self, characters, speaker_ids=None, style_mel=None):
         B = characters.size(0)
         inputs = self.embedding(characters)
+        self._init_states()
+        self.compute_speaker_embedding(speaker_ids)
+        if self.num_speakers > 1:
+            inputs = self._concat_speaker_embedding(inputs,
+                                                self.speaker_embeddings)
         encoder_outputs = self.encoder(inputs)
-        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
-                                                      speaker_ids)
+        if self.num_speakers > 1:
+            encoder_outputs = self._concat_speaker_embedding(encoder_outputs,
+                                                        self.speaker_embeddings)
         if style_mel is not None:
             gst_outputs = self.gst(style_mel)
             gst_outputs = gst_outputs.expand(-1, encoder_outputs.size(1), -1)
-            encoder_outputs = encoder_outputs + gst_outputs
+            encoder_outputs = self._concat_speaker_embedding(encoder_outputs,
+                                                             gst_outputs)
         mel_outputs, alignments, stop_tokens = self.decoder.inference(
-            encoder_outputs)
+            encoder_outputs, self.speaker_embeddings_projected)
         mel_outputs = mel_outputs.view(B, -1, self.mel_dim)
         linear_outputs = self.postnet(mel_outputs)
         linear_outputs = self.last_linear(linear_outputs)
         return mel_outputs, linear_outputs, alignments, stop_tokens
-
-    def _add_speaker_embedding(self, encoder_outputs, speaker_ids):
-        if hasattr(self, "speaker_embedding") and speaker_ids is not None:
-            speaker_embeddings = self.speaker_embedding(speaker_ids)
-
-            speaker_embeddings.unsqueeze_(1)
-            speaker_embeddings = speaker_embeddings.expand(encoder_outputs.size(0),
-                                                           encoder_outputs.size(1),
-                                                           -1)
-            encoder_outputs = encoder_outputs + speaker_embeddings
-        return encoder_outputs
