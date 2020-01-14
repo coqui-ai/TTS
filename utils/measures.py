b9109ca748901c3ed1f137fd10999b1a0d0376af
root <root@sp-mlc3-5423-0.mlc>
2020-01-14 02:27:09 +0100

    binary measure

--- utils/measures.py
+++ utils/measures.py
@@ -1,11 +1,18 @@
+import torch
 
-def alignment_diagonal_score(alignments):
+
+def alignment_diagonal_score(alignments, binary=False):
     """
     Compute how diagonal alignment predictions are. It is useful
     to measure the alignment consistency of a model
     Args:
         alignments (torch.Tensor): batch of alignments.
+        binary (bool): if True, ignore scores and consider attention
+        as a binary mask.
     Shape:
         alignments : batch x decoder_steps x encoder_steps
     """
-    return alignments.max(dim=1)[0].mean(dim=1).mean(dim=0).item()
+    if binary:
+        return torch.clamp(alignments.max(dim=1)[0], max=1.0).mean(dim=1).mean(dim=0).item()
+    else:
+        return alignments.max(dim=1)[0].mean(dim=1).mean(dim=0).item()
