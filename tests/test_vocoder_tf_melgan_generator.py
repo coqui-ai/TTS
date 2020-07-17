320bc294968f745d28458a33865030258ab3c741
erogol <erogol@hotmail.com>
2020-07-17 11:36:36 +0200

    pwgan files

---
+++ tests/test_vocoder_tf_melgan_generator.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+
+from TTS.vocoder.tf.models.melgan_generator import MelganGenerator
+
+def test_melgan_generator():
+    hop_length = 256
+    model = MelganGenerator()
+    dummy_input = tf.random.uniform((4, 80, 64))
+    output = model(dummy_input, training=False)
+    assert np.all(output.shape == (4, 1, 64 * hop_length)), output.shape
+
