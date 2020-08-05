07c961382fcc1882a80e4ef65e3c9a357928d49d
Edresson <edresson1@gmail.com>
2020-08-05 08:43:27 -0300

    add support for CorentinJ Speaker encoder and add notebook for extract embeddings

--- mozilla_voice_tts/speaker_encoder/model.py
+++ mozilla_voice_tts/speaker_encoder/model.py
@@ -16,15 +16,33 @@ class LSTMWithProjection(nn.Module):
         o, (_, _) = self.lstm(x)
         return self.linear(o)
 
+class LSTMWithoutProjection(nn.Module):
+    def __init__(self, input_dim, lstm_dim, proj_dim, num_lstm_layers):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=input_dim,
+                            hidden_size=lstm_dim,
+                            num_layers=num_lstm_layers,
+                            batch_first=True)
+        self.linear = nn.Linear(lstm_dim, proj_dim, bias=True)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        _, (hidden, _) = self.lstm(x)
+        return self.relu(self.linear(hidden[-1]))
 
 class SpeakerEncoder(nn.Module):
-    def __init__(self, input_dim, proj_dim=256, lstm_dim=768, num_lstm_layers=3):
+    def __init__(self, input_dim, proj_dim=256, lstm_dim=768, num_lstm_layers=3, use_lstm_with_projection=True):
         super().__init__()
+        self.use_lstm_with_projection = use_lstm_with_projection
         layers = []
-        layers.append(LSTMWithProjection(input_dim, lstm_dim, proj_dim))
-        for _ in range(num_lstm_layers - 1):
-            layers.append(LSTMWithProjection(proj_dim, lstm_dim, proj_dim))
-        self.layers = nn.Sequential(*layers)
+        # choise LSTM layer
+        if use_lstm_with_projection:
+            layers.append(LSTMWithProjection(input_dim, lstm_dim, proj_dim))
+            for _ in range(num_lstm_layers - 1):
+                layers.append(LSTMWithProjection(proj_dim, lstm_dim, proj_dim))
+            self.layers = nn.Sequential(*layers)
+        else:
+            self.layers = LSTMWithoutProjection(input_dim, lstm_dim, proj_dim, num_lstm_layers)
+
         self._init_layers()
 
     def _init_layers(self):
@@ -37,12 +55,18 @@ class SpeakerEncoder(nn.Module):
     def forward(self, x):
         # TODO: implement state passing for lstms
         d = self.layers(x)
-        d = torch.nn.functional.normalize(d[:, -1], p=2, dim=1)
+        if self.use_lstm_with_projection:
+            d = torch.nn.functional.normalize(d[:, -1], p=2, dim=1)
+        else:
+            d = torch.nn.functional.normalize(d, p=2, dim=1)
         return d
 
     def inference(self, x):
         d = self.layers.forward(x)
-        d = torch.nn.functional.normalize(d[:, -1], p=2, dim=1)
+        if self.use_lstm_with_projection:
+            d = torch.nn.functional.normalize(d[:, -1], p=2, dim=1)
+        else:
+            d = torch.nn.functional.normalize(d, p=2, dim=1)
         return d
 
     def compute_embedding(self, x, num_frames=160, overlap=0.5):
