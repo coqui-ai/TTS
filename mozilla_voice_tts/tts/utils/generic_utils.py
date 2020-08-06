027c83b1b4072aa91217281da93ba414e082448c
erogol <erogol@hotmail.com>
2020-08-06 14:23:17 +0200

    remove redundant arguments

--- mozilla_voice_tts/tts/utils/generic_utils.py
+++ mozilla_voice_tts/tts/utils/generic_utils.py
@@ -253,18 +253,6 @@ def check_config(c):
     check_argument('use_external_speaker_embedding_file', c, restricted=True, val_type=bool)
     check_argument('external_speaker_embedding_file', c, restricted=True, val_type=str)
     check_argument('use_gst', c, restricted=True, val_type=bool)
-    check_argument('gst_style_input', c, restricted=True, val_type=str)
-    check_argument('gst', c, restricted=True, val_type=dict)
-    check_argument('gst_embedding_dim', c['gst'], restricted=True, val_type=int, min_val=1)
-    check_argument('gst_num_heads', c['gst'], restricted=True, val_type=int, min_val=1)
-    check_argument('gst_style_tokens', c['gst'], restricted=True, val_type=int, min_val=1)
-
-    check_argument('gst', c, restricted=True, val_type=dict)
-    check_argument('gst_style_input', c['gst'], restricted=True, val_type=[str, dict])
-    check_argument('gst_embedding_dim', c['gst'], restricted=True, val_type=int, min_val=0, max_val=1000)
-    check_argument('gst_num_heads', c['gst'], restricted=True, val_type=int, min_val=2, max_val=10)
-    check_argument('gst_style_tokens', c['gst'], restricted=True, val_type=int, min_val=1, max_val=1000)
-
     check_argument('gst', c, restricted=True, val_type=dict)
     check_argument('gst_style_input', c['gst'], restricted=True, val_type=[str, dict])
     check_argument('gst_embedding_dim', c['gst'], restricted=True, val_type=int, min_val=0, max_val=1000)
