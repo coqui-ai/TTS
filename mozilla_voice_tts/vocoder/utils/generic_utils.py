9e33ab9f83b86b09823606554d591e00a02e0743
erogol <erogol@hotmail.com>
2020-08-17 13:56:10 +0200

    remove redundant arguments of pwgan

--- mozilla_voice_tts/vocoder/utils/generic_utils.py
+++ mozilla_voice_tts/vocoder/utils/generic_utils.py
@@ -78,11 +78,9 @@ def setup_generator(c):
             gate_channels=128,
             skip_channels=64,
             aux_channels=c.audio['num_mels'],
-            aux_context_window=c['conv_pad'],
             dropout=0.0,
             bias=True,
             use_weight_norm=True,
-            upsample_conditional_features=True,
             upsample_factors=c.generator_model_params['upsample_factors'])
     return model
 
