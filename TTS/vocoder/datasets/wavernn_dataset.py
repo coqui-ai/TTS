d7cda2df64bf7b1bd5fee9a1726a88df5b031d67
sanjaesc <alexx.korolev@gmail.com>
2020-10-25 09:45:37 +0100

    compute audio feat on dataload

--- TTS/vocoder/datasets/wavernn_dataset.py
+++ TTS/vocoder/datasets/wavernn_dataset.py
@@ -1,11 +1,13 @@
 import torch
 import numpy as np
 from torch.utils.data import Dataset
+from multiprocessing import Manager
 
 
 class WaveRNNDataset(Dataset):
     """
-    WaveRNN Dataset searchs for all the wav files under root path.
+    WaveRNN Dataset searchs for all the wav files under root path
+    and converts them to acoustic features on the fly.
     """
 
     def __init__(self,
@@ -15,16 +17,19 @@ class WaveRNNDataset(Dataset):
                  hop_len,
                  pad,
                  mode,
+                 mulaw,
                  is_training=True,
                  verbose=False,
                  ):
 
         self.ap = ap
+        self.compute_feat = not isinstance(items[0], (tuple, list))
         self.item_list = items
         self.seq_len = seq_len
         self.hop_len = hop_len
         self.pad = pad
         self.mode = mode
+        self.mulaw = mulaw
         self.is_training = is_training
         self.verbose = verbose
 
@@ -36,22 +41,47 @@ class WaveRNNDataset(Dataset):
         return item
 
     def load_item(self, index):
-        wavpath, feat_path = self.item_list[index]
-        m = np.load(feat_path.replace("/quant/", "/mel/"))
-        # x = self.wav_cache[index]
-        if m.shape[-1] < 5:
-            print(" [!] Instance is too short! : {}".format(wavpath))
-            self.item_list[index] = self.item_list[index + 1]
-            feat_path = self.item_list[index]
-            m = np.load(feat_path.replace("/quant/", "/mel/"))
-        if self.mode in ["gauss", "mold"]:
-            # x = np.load(feat_path.replace("/mel/", "/quant/"))
-            x = self.ap.load_wav(wavpath)
-        elif isinstance(self.mode, int):
-            x = np.load(feat_path.replace("/mel/", "/quant/"))
+        """
+        load (audio, feat) couple if feature_path is set
+        else compute it on the fly
+        """
+        if self.compute_feat:
+
+            wavpath = self.item_list[index]
+            audio = self.ap.load_wav(wavpath)
+            mel = self.ap.melspectrogram(audio)
+
+            if mel.shape[-1] < 5:
+                print(" [!] Instance is too short! : {}".format(wavpath))
+                self.item_list[index] = self.item_list[index + 1]
+                audio = self.ap.load_wav(wavpath)
+                mel = self.ap.melspectrogram(audio)
+            if self.mode in ["gauss", "mold"]:
+                x_input = audio
+            elif isinstance(self.mode, int):
+                x_input = (self.ap.mulaw_encode(audio, qc=self.mode)
+                           if self.mulaw else self.ap.quantize(audio, bits=self.mode))
+            else:
+                raise RuntimeError("Unknown dataset mode - ", self.mode)
+
         else:
-            raise RuntimeError("Unknown dataset mode - ", self.mode)
-        return m, x
+
+            wavpath, feat_path = self.item_list[index]
+            mel = np.load(feat_path.replace("/quant/", "/mel/"))
+
+            if mel.shape[-1] < 5:
+                print(" [!] Instance is too short! : {}".format(wavpath))
+                self.item_list[index] = self.item_list[index + 1]
+                feat_path = self.item_list[index]
+                mel = np.load(feat_path.replace("/quant/", "/mel/"))
+            if self.mode in ["gauss", "mold"]:
+                x_input = self.ap.load_wav(wavpath)
+            elif isinstance(self.mode, int):
+                x_input = np.load(feat_path.replace("/mel/", "/quant/"))
+            else:
+                raise RuntimeError("Unknown dataset mode - ", self.mode)
+
+        return mel, x_input
 
     def collate(self, batch):
         mel_win = self.seq_len // self.hop_len + 2 * self.pad
@@ -79,10 +109,8 @@ class WaveRNNDataset(Dataset):
         elif isinstance(self.mode, int):
             coarse = np.stack(coarse).astype(np.int64)
             coarse = torch.LongTensor(coarse)
-            x_input = (
-                2 * coarse[:, : self.seq_len].float() /
-                (2 ** self.mode - 1.0) - 1.0
-            )
+            x_input = (2 * coarse[:, : self.seq_len].float() /
+                       (2 ** self.mode - 1.0) - 1.0)
         y_coarse = coarse[:, 1:]
         mels = torch.FloatTensor(mels)
         return x_input, mels, y_coarse
