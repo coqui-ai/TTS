ef04d7fae7331897db6ac38069de3095538714e3
erogol <erogol@hotmail.com>
2020-10-30 14:08:41 +0100

    bug fix for wavernn training

--- TTS/bin/train_vocoder_wavernn.py
+++ TTS/bin/train_vocoder_wavernn.py
@@ -509,7 +509,7 @@ if __name__ == "__main__":
             new_fields["restore_path"] = args.restore_path
         new_fields["github_branch"] = get_git_branch()
         copy_config_file(
-            args.config_path, os.path.join(OUT_PATH, "c.json"), new_fields
+            args.config_path, os.path.join(OUT_PATH, "config.json"), new_fields
         )
         os.chmod(AUDIO_PATH, 0o775)
         os.chmod(OUT_PATH, 0o775)
