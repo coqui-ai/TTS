1faf565e3ae2270e7c603368afa7e2234a5877f2
root <erogol@hotmail.com>
2021-01-20 02:10:56 +0000

    add load_checkpoint func to tts models

--- TTS/tts/models/speedy_speech.py
+++ TTS/tts/models/speedy_speech.py
@@ -190,3 +190,10 @@ class SpeedySpeech(nn.Module):
         y_lengths = o_dr.sum(1)
         o_de, attn= self._forward_decoder(o_en, o_en_dp, o_dr, x_mask, y_lengths, g=g)
         return o_de, attn
+
+    def load_checkpoint(self, config, checkpoint_path, eval=False):
+        state = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+        self.load_state_dict(state['model'])
+        if eval:
+            self.eval()
+            assert not self.training
