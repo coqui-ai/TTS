f44ca0673c234d1699f7ee129546bac8239acd18
Eren Gölge <erogol@hotmail.com>
2021-01-29 15:17:29 +0000

    docstring hubconf

--- hubconf.py
+++ hubconf.py
@@ -8,8 +8,22 @@ from TTS.utils.synthesizer import Synthesizer
 from TTS.utils.manage import ModelManager
 
 
+def tts(model_name='tts_models/en/ljspeech/tacotron2-DCA', vocoder_name='vocoder_models/en/ljspeech/mulitband-melgan'):
+    """TTS entry point for PyTorch Hub that provides a Synthesizer object to synthesize speech from a give text.
 
-def tts(model_name='tts_models/en/ljspeech/tacotron2-DCA', vocoder_name='vocoder_models/en/ljspeech/mulitband-melgan', pretrained=True):
+    Example:
+        >>> synthesizer = torch.hub.load('mozilla/TTS', 'tts', source='github')
+        >>> wavs = synthesizer.tts("This is a test! This is also a test!!")
+            wavs - is a list of values of the synthesized speech.
+        
+    Args:
+        model_name (str, optional): One of the model names from .model.json. Defaults to 'tts_models/en/ljspeech/tacotron2-DCA'.
+        vocoder_name (str, optional): One of the model names from .model.json. Defaults to 'vocoder_models/en/ljspeech/mulitband-melgan'.
+        pretrained (bool, optional): [description]. Defaults to True.
+
+    Returns:
+        TTS.utils.synthesizer.Synthesizer: Synthesizer object wrapping both vocoder and tts models.
+    """    
     manager = ModelManager()
   
     model_path, config_path = manager.download_model(model_name)
@@ -21,6 +35,5 @@ def tts(model_name='tts_models/en/ljspeech/tacotron2-DCA', vocoder_name='vocoder
 
 
 if __name__ == '__main__':
-    # synthesizer = torch.hub.load('/data/rw/home/projects/TTS/TTS', 'tts', source='local')
     synthesizer = torch.hub.load('mozilla/TTS:hub_conf', 'tts', source='github')
     synthesizer.tts("This is a test!")
