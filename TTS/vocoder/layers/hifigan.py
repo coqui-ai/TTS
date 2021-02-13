4493feb95ca8894479290bf5d704ddf613131f72
rishikksh20 <rishikksh20@gmail.com>
2021-02-14 01:09:55 +0530

    Add HiFi-GAN v1 generator and discriminator classes

---
+++ TTS/vocoder/layers/hifigan.py
@@ -0,0 +1,40 @@
+from torch import nn
+
+
+class ResStack(nn.Module):
+    def __init__(self, kernel, channel, padding, dilations = [1, 3, 5]):
+        super(ResStack, self).__init__()
+        resstack = []
+        for dilation in dilations:
+                resstack += [
+                    nn.LeakyReLU(0.2),
+                    nn.ReflectionPad1d(dilation),
+                    nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=kernel, dilation=dilation)),
+                    nn.LeakyReLU(0.2),
+                    nn.ReflectionPad1d(padding),
+                    nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1)),
+                ]
+        self.resstack = nn.Sequential(*resstack)
+
+        self.shortcut = nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1))
+
+    def forward(self, x):
+      x1 = self.shortcut(x)
+      x2 = self.resstack(x)
+      return  x1 + x2
+
+    def remove_weight_norm(self):
+        nn.utils.remove_weight_norm(self.shortcut)
+
+class MRF(nn.Module):
+    def __init__(self, kernels, channel, dilations = [[1,1], [3,1], [5,1]]):
+        super(MRF, self).__init__()
+        self.resblock1 = ResStack(kernels[0], channel, 0)
+        self.resblock2 = ResStack(kernels[1], channel, 6)
+        self.resblock3 = ResStack(kernels[2], channel, 12)
+
+    def forward(self, x):
+      x1 = self.resblock1(x)
+      x2 = self.resblock2(x)
+      x3 = self.resblock3(x)
+      return x1 + x2 + x3
---
+++ TTS/vocoder/models/hifigan_generator.py
@@ -0,0 +1,39 @@
+from torch import nn
+from TTS.vocoder.layers.hifigan import MRF
+
+
+class Generator(nn.Module):
+
+    def __init__(self, input_channel=80, hu=512, ku=[16, 16, 4, 4], kr=[3, 7, 11], Dr=[1, 3, 5]):
+        super(Generator, self).__init__()
+        self.input = nn.Sequential(
+            nn.ReflectionPad1d(3),
+            nn.utils.weight_norm(nn.Conv1d(input_channel, hu, kernel_size=7))
+        )
+
+        generator = []
+
+        for k in ku:
+            inp = hu
+            out = int(inp / 2)
+            generator += [
+                nn.LeakyReLU(0.2),
+                nn.ConvTranspose1d(inp, out, k, k // 2),
+                MRF(kr, out, Dr)
+            ]
+            hu = out
+        self.generator = nn.Sequential(*generator)
+
+        self.output = nn.Sequential(
+            nn.LeakyReLU(0.2),
+            nn.ReflectionPad1d(3),
+            nn.utils.weight_norm(nn.Conv1d(hu, 1, kernel_size=7, stride=1)),
+            nn.Tanh()
+
+        )
+
+    def forward(self, x):
+        x1 = self.input(x)
+        x2 = self.generator(x1)
+        out = self.output(x2)
+        return out
