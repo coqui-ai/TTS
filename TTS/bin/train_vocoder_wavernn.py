2451a813a24169a071ad8add4b3bd2c0389230d5
gerazov <gerazov@feit.ukim.edu.mk>
2021-02-15 18:40:17 +0100

    refactored keep_all_best

--- TTS/bin/train_vocoder_wavernn.py
+++ TTS/bin/train_vocoder_wavernn.py
@@ -426,8 +426,8 @@ def main(args):  # pylint: disable=redefined-outer-name
         best_loss = torch.load(args.best_path,
                                map_location='cpu')['model_loss']
         print(f" > Starting with loaded last best loss {best_loss}.")
-    keep_best = c.get('keep_best', False)
-    keep_after = c.get('keep_after', 10000)  # void if keep_best False
+    keep_all_best = c.get('keep_all_best', False)
+    keep_after = c.get('keep_after', 10000)  # void if keep_all_best False
 
     global_step = args.restore_step
     for epoch in range(0, c.epochs):
@@ -450,7 +450,7 @@ def main(args):  # pylint: disable=redefined-outer-name
             global_step,
             epoch,
             OUT_PATH,
-            keep_best=keep_best,
+            keep_all_best=keep_all_best,
             keep_after=keep_after,
             model_losses=eval_avg_loss_dict,
             scaler=scaler.state_dict() if c.mixed_precision else None
