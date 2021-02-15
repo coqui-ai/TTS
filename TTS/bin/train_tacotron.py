a6b49d129f01223a00ec8b069a01b63fab8f5bad
gerazov <gerazov@feit.ukim.edu.mk>
2021-02-15 18:40:17 +0100

    refactored keep_all_best

--- TTS/bin/train_tacotron.py
+++ TTS/bin/train_tacotron.py
@@ -595,8 +595,8 @@ def main(args):  # pylint: disable=redefined-outer-name
         best_loss = torch.load(args.best_path,
                                map_location='cpu')['model_loss']
         print(f" > Starting with loaded last best loss {best_loss}.")
-    keep_best = c.get('keep_best', False)
-    keep_after = c.get('keep_after', 10000)  # void if keep_best False
+    keep_all_best = c.get('keep_all_best', False)
+    keep_after = c.get('keep_after', 10000)  # void if keep_all_best False
 
     # define data loaders
     train_loader = setup_loader(ap,
@@ -648,7 +648,7 @@ def main(args):  # pylint: disable=redefined-outer-name
             epoch,
             c.r,
             OUT_PATH,
-            keep_best=keep_best,
+            keep_all_best=keep_all_best,
             keep_after=keep_after,
             scaler=scaler.state_dict() if c.mixed_precision else None
         )
