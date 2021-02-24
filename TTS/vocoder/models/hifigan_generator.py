af97fd97e4c89eac5b31107672a6c291fde52c0c
rishikksh20 <rishikksh20@gmail.com>
2021-02-24 08:01:51 +0530

    * Format the model definition * Update code and integrate training code

--- TTS/vocoder/models/hifigan_generator.py
+++ TTS/vocoder/models/hifigan_generator.py
@@ -4,30 +4,31 @@ from TTS.vocoder.layers.hifigan import MRF
 
 class Generator(nn.Module):
 
-    def __init__(self, input_channel=80, hu=512, ku=[16, 16, 4, 4], kr=[3, 7, 11], Dr=[1, 3, 5]):
+    def __init__(self, in_channels=80, out_channels=1, base_channels=512, upsample_kernel=[16, 16, 4, 4],
+                 resblock_kernel_sizes=[3, 7, 11], resblock_dilation_sizes=[1, 3, 5]):
         super(Generator, self).__init__()
         self.input = nn.Sequential(
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(input_channel, hu, kernel_size=7))
+            nn.utils.weight_norm(nn.Conv1d(in_channels, base_channels, kernel_size=7))
         )
 
         generator = []
 
-        for k in ku:
-            inp = hu
+        for k in upsample_kernel:
+            inp = base_channels
             out = int(inp / 2)
             generator += [
                 nn.LeakyReLU(0.2),
                 nn.utils.weight_norm(nn.ConvTranspose1d(inp, out, k, k//2)),
-                MRF(kr, out, Dr)
+                MRF(resblock_kernel_sizes, out, resblock_dilation_sizes)
             ]
-            hu = out
+            base_channels = out
         self.generator = nn.Sequential(*generator)
 
         self.output = nn.Sequential(
             nn.LeakyReLU(0.2),
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(hu, 1, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(base_channels, out_channels, kernel_size=7, stride=1)),
             nn.Tanh()
 
         )
