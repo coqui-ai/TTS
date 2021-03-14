39b88dac96056f72160e5a6b0bd7eae36c8759da
MitchellProductions <37678351+MitchellProductions@users.noreply.github.com>
2021-03-14 10:00:40 -0600

    Update coqui_tts_gui.py

--- TTS/bin/coqui_tts_gui.py
+++ TTS/bin/coqui_tts_gui.py
@@ -18,8 +18,8 @@ from pathlib import Path, PurePath
 import os
 
 def generate():
-    if inputbox.get("1.0","end-1c") == "":
-        messagebox.showerror(message = "TTS will give a division by zero error if the text field is blank.")
+    if inputbox.get("1.0", "end-1c") == "":
+        messagebox.showerror(message="TTS will give a division by zero error if the text field is blank.")
     else:
         if not os.path.exists('coqui-tts-output'):
             try:
@@ -38,17 +38,17 @@ def generate():
         model_name = 'tts_models/' + ttsmodelbox.get()
         print(f'model_name is {model_name}')
         # for dev
-        model_path, config_path, model_item = manager.download_model(model_name)
+        #model_path, config_path, model_item = manager.download_model(model_name)
         # for master
-        #model_path, config_path = manager.download_model(model_name)
+        model_path, config_path = manager.download_model(model_name)
         vocoder_name = 'vocoder_models/' + vocodermodelbox.get()
         print(f'vocoder_name is {vocoder_name}')
         # for dev
-        vocoder_path, vocoder_config_path, model_item = manager.download_model(vocoder_name)
+        #vocoder_path, vocoder_config_path, model_item = manager.download_model(vocoder_name)
         # for master
-        #vocoder_path, vocoder_config_path = manager.download_model(vocoder_name)
+        vocoder_path, vocoder_config_path = manager.download_model(vocoder_name)
         synthesizer = Synthesizer(model_path, config_path, vocoder_path, vocoder_config_path, cudacheckbutton.instate(['selected']))
-        wav = synthesizer.tts(inputbox.get("1.0","end-1c"))
+        wav = synthesizer.tts(inputbox.get("1.0", "end-1c"))
         synthesizer.save_wav(wav, "coqui-tts-output/generated.wav")
         playsound("coqui-tts-output/generated.wav")
         generatebutton.config(state="enabled")
@@ -56,29 +56,29 @@ def generate():
         print("All done!")
 
 def savetext():
-    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes = [("Text files", ".txt")])
+    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes=[("Text files", ".txt")])
     if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
         return
-    text2save = str(inputbox.get("1.0","end-1c")) # starts from `1.0`, not `0.0`
+    text2save = str(inputbox.get("1.0", "end-1c")) # starts from `1.0`, not `0.0`
     f.write(text2save)
     f.close() # `()` was missing.
     inputbox.edit_modified(False)
 
 def savetextandopen():
-    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes = [("Text files", ".txt")])
+    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes=[("Text files", ".txt")])
     if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
         return
-    text2save = str(inputbox.get("1.0","end-1c")) # starts from `1.0`, not `0.0`
+    text2save = str(inputbox.get("1.0", "end-1c")) # starts from `1.0`, not `0.0`
     f.write(text2save)
     f.close() # `()` was missing.
     inputbox.edit_modified(False)
     opentext()
 
 def exportaudio():
-    if inputbox.get("1.0","end-1c") == "":
-        messagebox.showerror(message = "TTS will give a division by zero error if the text field is blank.")
+    if inputbox.get("1.0", "end-1c") == "":
+        messagebox.showerror(message="TTS will give a division by zero error if the text field is blank.")
     else:
-        f = filedialog.asksaveasfile(mode='a', defaultextension=".wav", filetypes = [("Wave files", ".wav")])
+        f = filedialog.asksaveasfile(mode='a', defaultextension=".wav", filetypes=[("Wave files", ".wav")])
         if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
             return
     generatebutton.config(state="disabled")
@@ -92,34 +92,34 @@ def exportaudio():
     model_name = 'tts_models/' + ttsmodelbox.get()
     print(f'model_name is {model_name}')
     # for dev
-    model_path, config_path, model_item = manager.download_model(model_name)
+    #model_path, config_path, model_item = manager.download_model(model_name)
     # for master
-    #model_path, config_path = manager.download_model(model_name)
+    model_path, config_path = manager.download_model(model_name)
     vocoder_name = 'vocoder_models/' + vocodermodelbox.get()
     print(f'vocoder_name is {vocoder_name}')
     # for dev
-    vocoder_path, vocoder_config_path, model_item = manager.download_model(vocoder_name)
+    #vocoder_path, vocoder_config_path, model_item = manager.download_model(vocoder_name)
     # for master
-    #vocoder_path, vocoder_config_path = manager.download_model(vocoder_name)
+    vocoder_path, vocoder_config_path = manager.download_model(vocoder_name)
     synthesizer = Synthesizer(model_path, config_path, vocoder_path, vocoder_config_path, cudacheckbutton.instate(['selected']))
-    wav = synthesizer.tts(inputbox.get("1.0","end-1c"))
+    wav = synthesizer.tts(inputbox.get("1.0", "end-1c"))
     synthesizer.save_wav(wav, str(f.name))
     generatebutton.config(state="enabled")
     exportbutton.config(state="enabled")
     print("All done!")
 
 def opentext():
-    file = filedialog.askopenfile(mode='rt', defaultextension=".txt", filetypes = [("Text files", ".txt")])
+    file = filedialog.askopenfile(mode='rt', defaultextension=".txt", filetypes=[("Text files", ".txt")])
     if file is None: # asksaveasfile return `None` if dialog closed with "cancel".
         return
     contents = file.read()
-    inputbox.insert('1.0',contents)
+    inputbox.insert('1.0', contents)
     inputbox.edit_modified(False)
     file.close()
 
 def checkopentext():
     if inputbox.edit_modified() == True:
-        response=messagebox.askyesnocancel(message = "You have unsaved changes. Do you want to save?")
+        response=messagebox.askyesnocancel(message="You have unsaved changes. Do you want to save?")
         if response == True:
             savetextandopen()
         elif response == False:
@@ -135,7 +135,7 @@ window.geometry('')
 window.title("Coqui TTS GUI")
 window.resizable(False, False)
 # Label 
-ttk.Label(window, text="Enter text here", font = ("Tahoma", 10)).grid(column=0, columnspan=3, row=12, padx=10, pady=12)
+ttk.Label(window, text="Enter text here", font=("Tahoma", 10)).grid(column=0, columnspan=3, row=12, padx=10, pady=12)
 
 # Text
 inputbox = scrolledtext.ScrolledText(window, height=15, width=70, undo=True)
@@ -148,10 +148,10 @@ n = tk.StringVar()
 ttsmodelbox = ttk.Combobox(window, width=32, textvariable=n, state="readonly") 
 # Adding combobox drop down list 
 ttsmodelbox['values'] = ('en/ljspeech/glow-tts',  
-                            'en/ljspeech/tacotron2-DCA', 
-                            'en/ljspeech/speedy-speech-wn', 
-                            'es/mai/tacotron2-DDC', 
-                            'fr/mai/tacotron2-DDC') 
+                        'en/ljspeech/tacotron2-DCA', 
+                        'en/ljspeech/speedy-speech-wn', 
+                        'es/mai/tacotron2-DDC', 
+                        'fr/mai/tacotron2-DDC') 
   
 ttsmodelbox.grid(column=0, row=15, padx=10, pady=12)
 ttsmodelbox.current(0)
@@ -173,8 +173,8 @@ cudacheckbutton.state(['!selected'])
 
 # Adding combobox drop down list 
 vocodermodelbox['values'] = ('universal/libri-tts/wavegrad',  
-                                'universal/libri-tts/fullband-melgan', 
-                                'en/ljspeech/multiband-melgan') 
+                            'universal/libri-tts/fullband-melgan', 
+                            'en/ljspeech/multiband-melgan') 
   
 vocodermodelbox.grid(column=2, row=15, padx=10, pady=12)
 vocodermodelbox.current(0)
