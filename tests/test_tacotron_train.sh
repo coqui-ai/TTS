aa8bb815a743d3d14fa07c62434b5cdcee87b938
Eren Gölge <egolge@coqui.ai>
2021-03-15 11:28:06 +0100

    fix mozilla/TTS#685

--- tests/test_tacotron_train.sh
+++ tests/test_tacotron_train.sh
@@ -2,8 +2,19 @@
 set -xe
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
+
 # run training
 CUDA_VISIBLE_DEVICES="" python TTS/bin/train_tacotron.py --config_path $BASEDIR/inputs/test_tacotron_config.json
+# find the training folder
+LATEST_FOLDER=$(ls $BASEDIR/train_outputs/| sort | tail -1)
+echo $LATEST_FOLDER
+# continue the previous training
+CUDA_VISIBLE_DEVICES=""  python TTS/bin/train_tacotron.py --continue_path $BASEDIR/train_outputs/$LATEST_FOLDER
+# remove all the outputs
+rm -rf $BASEDIR/train_outputs/
+
+# run Tacotron bi-directional decoder
+CUDA_VISIBLE_DEVICES="" python TTS/bin/train_tacotron.py --config_path $BASEDIR/inputs/test_tacotron_bd_config.json
 # find the training folder
 LATEST_FOLDER=$(ls $BASEDIR/train_outputs/| sort | tail -1)
 echo $LATEST_FOLDER
