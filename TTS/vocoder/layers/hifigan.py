f94622b5954f8e65395773b9c299229777e06321
Eren Gölge <egolge@coqui.ai>
2021-04-05 11:32:51 +0200

    hifigan implementation update

--- TTS/vocoder/layers/hifigan.py
+++ TTS/vocoder/layers/hifigan.py
@@ -2,26 +2,32 @@ from torch import nn
 
 
 class ResStack(nn.Module):
-    def __init__(self, kernel, channel, padding, dilations = [1, 3, 5]):
+    def __init__(self, kernel, channel, padding, dilations=[1, 3, 5]):
         super(ResStack, self).__init__()
         resstack = []
         for dilation in dilations:
-                resstack += [
-                    nn.LeakyReLU(0.2),
-                    nn.ReflectionPad1d(dilation),
-                    nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=kernel, dilation=dilation)),
-                    nn.LeakyReLU(0.2),
-                    nn.ReflectionPad1d(padding),
-                    nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1)),
-                ]
+            resstack += [
+                nn.LeakyReLU(0.2),
+                nn.ReflectionPad1d(dilation),
+                nn.utils.weight_norm(
+                    nn.Conv1d(channel,
+                              channel,
+                              kernel_size=kernel,
+                              dilation=dilation)),
+                nn.LeakyReLU(0.2),
+                nn.ReflectionPad1d(padding),
+                nn.utils.weight_norm(nn.Conv1d(channel, channel,
+                                               kernel_size=1)),
+            ]
         self.resstack = nn.Sequential(*resstack)
 
-        self.shortcut = nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1))
+        self.shortcut = nn.utils.weight_norm(
+            nn.Conv1d(channel, channel, kernel_size=1))
 
     def forward(self, x):
-      x1 = self.shortcut(x)
-      x2 = self.resstack(x)
-      return  x1 + x2
+        x1 = self.shortcut(x)
+        x2 = self.resstack(x)
+        return x1 + x2
 
     def remove_weight_norm(self):
         nn.utils.remove_weight_norm(self.shortcut)
@@ -32,18 +38,19 @@ class ResStack(nn.Module):
         nn.utils.remove_weight_norm(self.resstack[14])
         nn.utils.remove_weight_norm(self.resstack[17])
 
+
 class MRF(nn.Module):
-    def __init__(self, kernels, channel, dilations = [[1,1], [3,1], [5,1]]):
-        super(MRF, self).__init__()
-        self.resblock1 = ResStack(kernels[0], channel, 0)
-        self.resblock2 = ResStack(kernels[1], channel, 6)
-        self.resblock3 = ResStack(kernels[2], channel, 12)
+    def __init__(self, kernels, channel, dilations=[1, 3, 5]):
+        super().__init__()
+        self.resblock1 = ResStack(kernels[0], channel, 0, dilations)
+        self.resblock2 = ResStack(kernels[1], channel, 6, dilations)
+        self.resblock3 = ResStack(kernels[2], channel, 12, dilations)
 
     def forward(self, x):
-      x1 = self.resblock1(x)
-      x2 = self.resblock2(x)
-      x3 = self.resblock3(x)
-      return x1 + x2 + x3
+        x1 = self.resblock1(x)
+        x2 = self.resblock2(x)
+        x3 = self.resblock3(x)
+        return x1 + x2 + x3
 
     def remove_weight_norm(self):
         self.resblock1.remove_weight_norm()
--- TTS/vocoder/models/multi_period_discriminator.py
+++ TTS/vocoder/models/multi_period_discriminator.py
@@ -2,17 +2,18 @@ from torch import nn
 import torch.nn.functional as F
 from TTS.vocoder.models.melgan_multiscale_discriminator import MelganMultiscaleDiscriminator
 
-class PeriodDiscriminator(nn.Module):
 
+class PeriodDiscriminator(nn.Module):
     def __init__(self, period):
         super(PeriodDiscriminator, self).__init__()
         layer = []
         self.period = period
         inp = 1
         for l in range(4):
-            out = int(2 ** (5 + l + 1))
+            out = int(2**(5 + l + 1))
             layer += [
-                nn.utils.weight_norm(nn.Conv2d(inp, out, kernel_size=(5, 1), stride=(3, 1))),
+                nn.utils.weight_norm(
+                    nn.Conv2d(inp, out, kernel_size=(5, 1), stride=(3, 1))),
                 nn.LeakyReLU(0.2)
             ]
             inp = out
@@ -20,8 +21,7 @@ class PeriodDiscriminator(nn.Module):
         self.output = nn.Sequential(
             nn.utils.weight_norm(nn.Conv2d(out, 1024, kernel_size=(5, 1))),
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.Conv2d(1024, 1, kernel_size=(3, 1)))
-        )
+            nn.utils.weight_norm(nn.Conv2d(1024, 1, kernel_size=(3, 1))))
 
     def forward(self, x):
         batch_size = x.shape[0]
@@ -33,39 +33,41 @@ class PeriodDiscriminator(nn.Module):
         return self.output(out1)
 
 
-class MultiPeriodDiscriminator(nn.Module):
+class HifiDiscriminator(nn.Module):
     def __init__(self,
                  periods=[2, 3, 5, 7, 11],
                  in_channels=1,
                  out_channels=1,
                  num_scales=3,
                  kernel_sizes=(5, 3),
-                 base_channels=16,
+                 base_channels=64,
                  max_channels=1024,
-                 downsample_factors=(4, 4, 4),
+                 downsample_factors=(2, 2, 4, 4),
                  pooling_kernel_size=4,
                  pooling_stride=2,
                  pooling_padding=1):
-        super(MultiPeriodDiscriminator, self).__init__()
-        self.discriminators = nn.ModuleList([ PeriodDiscriminator(periods[0]),
-                                              PeriodDiscriminator(periods[1]),
-                                              PeriodDiscriminator(periods[2]),
-                                              PeriodDiscriminator(periods[3]),
-                                              PeriodDiscriminator(periods[4])
-                                            ])
+        super().__init__()
+        self.discriminators = nn.ModuleList([
+            PeriodDiscriminator(periods[0]),
+            PeriodDiscriminator(periods[1]),
+            PeriodDiscriminator(periods[2]),
+            PeriodDiscriminator(periods[3]),
+            PeriodDiscriminator(periods[4])
+        ])
 
         self.msd = MelganMultiscaleDiscriminator(
-                                             in_channels=in_channels,
-                                             out_channels=out_channels,
-                                             num_scales=num_scales,
-                                             kernel_sizes=kernel_sizes,
-                                             base_channels=base_channels,
-                                             max_channels=max_channels,
-                                             downsample_factors=downsample_factors,
-                                             pooling_kernel_size=pooling_kernel_size,
-                                             pooling_stride=pooling_stride,
-                                             pooling_padding=pooling_padding
-        )
+            in_channels=in_channels,
+            out_channels=out_channels,
+            num_scales=num_scales,
+            kernel_sizes=kernel_sizes,
+            base_channels=base_channels,
+            max_channels=max_channels,
+            downsample_factors=downsample_factors,
+            pooling_kernel_size=pooling_kernel_size,
+            pooling_stride=pooling_stride,
+            pooling_padding=pooling_padding,
+            groups_denominator=32,
+            max_groups=16)
 
     def forward(self, x):
         scores, feats = self.msd(x)
