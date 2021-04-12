fe4701907869f962c1f05894a08be7f388ca318a
Eren Gölge <egolge@coqui.ai>
2021-04-12 12:40:55 +0200

    small fixes

--- TTS/bin/train_vocoder_wavernn.py
+++ TTS/bin/train_vocoder_wavernn.py
@@ -24,10 +24,6 @@ from TTS.vocoder.utils.io import save_best_model, save_checkpoint
 
 # from torch.utils.data.distributed import DistributedSampler
 
-<<<<<<< HEAD
-
-=======
->>>>>>> reformat
 
 use_cuda, num_gpus = setup_torch_training_env(True, True)
 
