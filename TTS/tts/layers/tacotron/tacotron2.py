900aab6cd5a1b003f26ec1f9f28e0e39ddd04b5a
Adam Froghyar <adamfroghyar@gmail.com>
2021-04-30 10:45:54 +0200

    add whitespace

--- TTS/tts/layers/tacotron/tacotron2.py
+++ TTS/tts/layers/tacotron/tacotron2.py
@@ -310,6 +310,7 @@ class Decoder(nn.Module):
         memories = torch.cat((memory, memories), dim=0)
         memories = self._update_memory(memories)
         memories = self.prenet(memories)
+        
         self._init_states(inputs, mask=mask)
 
         outputs, stop_tokens, alignments = [], [], []
