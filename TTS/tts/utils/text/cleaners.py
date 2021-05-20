925c08cf95386c936e7e7f979f6b536b2440ec5e
Eren Gölge <egolge@coqui.ai>
2021-05-21 00:30:39 +0200

    replace unidecode with anyascii

--- TTS/tts/utils/text/cleaners.py
+++ TTS/tts/utils/text/cleaners.py
@@ -1,18 +1,6 @@
-"""
-Cleaners are transformations that run over the input text at both training and eval time.
-
-Cleaners can be selected by passing a comma-delimited list of cleaner names as the "cleaners"
-hyperparameter. Some cleaners are English-specific. You'll typically want to use:
-  1. "english_cleaners" for English text
-  2. "transliteration_cleaners" for non-English text that can be transliterated to ASCII using
-     the Unidecode library (https://pypi.python.org/pypi/Unidecode)
-  3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
-     the symbols in symbols.py to match your data).
-"""
-
 import re
 
-from unidecode import unidecode
+from anyascii import anyascii
 
 from TTS.tts.utils.text.chinese_mandarin.numbers import replace_numbers_to_characters_in_text
 
@@ -47,7 +35,7 @@ def collapse_whitespace(text):
 
 
 def convert_to_ascii(text):
-    return unidecode(text)
+    return anyascii(text)
 
 
 def remove_aux_symbols(text):
