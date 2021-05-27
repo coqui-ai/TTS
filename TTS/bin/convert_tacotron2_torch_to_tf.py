821a8ddfbde28b2d4091301b1df7d2fcbde0b229
Eren Gölge <egolge@coqui.ai>
2021-05-27 10:47:24 +0200

    update `setup_model.py` imports

--- TTS/bin/convert_tacotron2_torch_to_tf.py
+++ TTS/bin/convert_tacotron2_torch_to_tf.py
@@ -11,7 +11,7 @@ import torch
 from TTS.tts.tf.models.tacotron2 import Tacotron2
 from TTS.tts.tf.utils.convert_torch_to_tf_utils import compare_torch_tf, convert_tf_name, transfer_weights_torch_to_tf
 from TTS.tts.tf.utils.generic_utils import save_checkpoint
-from TTS.tts.utils.generic_utils import setup_model
+from TTS.tts.models import setup_model
 from TTS.tts.utils.text.symbols import phonemes, symbols
 from TTS.utils.io import load_config
 
--- tests/test_extract_tts_spectrograms.py
+++ tests/test_extract_tts_spectrograms.py
@@ -5,7 +5,7 @@ import torch
 
 from tests import get_tests_input_path, get_tests_output_path, run_cli
 from TTS.config import load_config
-from TTS.tts.utils.generic_utils import setup_model
+from TTS.tts.models import setup_model
 from TTS.tts.utils.text.symbols import phonemes, symbols
 
 torch.manual_seed(1)
