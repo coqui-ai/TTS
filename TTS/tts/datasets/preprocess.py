49c5e5d820e2413acb80c3b9004e5a5243d44f41
Eren Gölge <egolge@coqui.ai>
2021-06-02 11:42:38 +0200

    maket style japanese PR

--- TTS/tts/datasets/preprocess.py
+++ TTS/tts/datasets/preprocess.py
@@ -434,7 +434,7 @@ def kokoro(root_path, meta_file):
     with open(txt_file, "r") as ttf:
         for line in ttf:
             cols = line.split("|")
-            wav_file = os.path.join(root_path, "wavs", cols[0] + '.wav')
+            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
             text = cols[2].replace(" ", "")
             items.append([text, wav_file, speaker_name])
     return items
