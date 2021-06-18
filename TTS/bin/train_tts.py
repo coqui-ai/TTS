406642cd70df7b0705da020d1e32740919292601
Eren Gölge <egolge@coqui.ai>
2021-06-18 13:22:05 +0200

    Implement unified trainer

--- TTS/bin/train_tts.py
+++ TTS/bin/train_tts.py
@@ -1,27 +1,13 @@
-import os
 import sys
-import traceback
 
-from TTS.tts.trainer_tts import TrainerTTS
-from TTS.utils.arguments import init_training
-from TTS.utils.generic_utils import remove_experiment_folder
+from TTS.trainer import Trainer, init_training
 
 
 def main():
-    try:
-        args, config, output_path, _, c_logger, tb_logger = init_training(sys.argv)
-        trainer = TrainerTTS(args, config, c_logger, tb_logger, output_path=output_path)
-        trainer.fit()
-    except KeyboardInterrupt:
-        remove_experiment_folder(output_path)
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)  # pylint: disable=protected-access
-    except Exception:  # pylint: disable=broad-except
-        remove_experiment_folder(output_path)
-        traceback.print_exc()
-        sys.exit(1)
+    """Run 🐸TTS trainer from terminal. This is also necessary to run DDP training by ```distribute.py```"""
+    args, config, output_path, _, c_logger, tb_logger = init_training(sys.argv)
+    trainer = Trainer(args, config, output_path, c_logger, tb_logger, cudnn_benchmark=False)
+    trainer.fit()
 
 
 if __name__ == "__main__":
