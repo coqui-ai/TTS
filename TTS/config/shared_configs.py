e9a3f6d0311c1a69e1896a6f51e68835341099cd
Eren Gölge <egolge@coqui.ai>
2021-06-18 14:27:28 +0200

    fixup configs

--- TTS/config/shared_configs.py
+++ TTS/config/shared_configs.py
@@ -1,7 +1,7 @@
 from dataclasses import asdict, dataclass
 from typing import List
 
-from coqpit import MISSING, Coqpit, check_argument
+from coqpit import Coqpit, check_argument
 
 
 @dataclass
@@ -214,7 +214,7 @@ class BaseTrainingConfig(Coqpit):
             to 10000.
         num_loader_workers (int):
             Number of workers for training time dataloader.
-        num_val_loader_workers (int):
+        num_eval_loader_workers (int):
             Number of workers for evaluation time dataloader.
         output_path (str):
             Path for training output folder. The nonexist part of the given path is created automatically.
@@ -243,8 +243,8 @@ class BaseTrainingConfig(Coqpit):
     keep_all_best: bool = False
     keep_after: int = 10000
     # dataloading
-    num_loader_workers: int = MISSING
-    num_val_loader_workers: int = 0
+    num_loader_workers: int = None
+    num_eval_loader_workers: int = 0
     use_noise_augment: bool = False
     # paths
     output_path: str = None
