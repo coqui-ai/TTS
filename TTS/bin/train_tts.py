3446ef34c590615d053969d11dec3490acb3d95f
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-07-09 14:03:12 +0530

    Unified logger API

--- TTS/bin/train_tts.py
+++ TTS/bin/train_tts.py
@@ -5,8 +5,8 @@ from TTS.trainer import Trainer, init_training
 
 def main():
     """Run 🐸TTS trainer from terminal. This is also necessary to run DDP training by ```distribute.py```"""
-    args, config, output_path, _, c_logger, tb_logger, wandb_logger = init_training(sys.argv)
-    trainer = Trainer(args, config, output_path, c_logger, tb_logger, wandb_logger, cudnn_benchmark=False)
+    args, config, output_path, _, c_logger, dashboard_logger = init_training(sys.argv)
+    trainer = Trainer(args, config, output_path, c_logger, dashboard_logger, cudnn_benchmark=False)
     trainer.fit()
 
 
