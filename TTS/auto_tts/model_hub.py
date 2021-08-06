5aa900221f1e4c933c0ab45f0f6776dc15dd3279
logan <loganhartpersonal@gmail.com>
2021-08-05 22:31:19 -0400

    added SCGlowTts to model hub

--- TTS/auto_tts/model_hub.py
+++ TTS/auto_tts/model_hub.py
@@ -262,3 +262,98 @@ class Models:
             datasets=[dataset],
         )
         return config
+
+    def ScGlowTts(self, audio, dataset, encoder, speaker_file):
+        if encoder == "transformer":
+            encoder_type = "rel_pos_transformer"
+            encoder_params = {
+                "kernel_size": 3,
+                "dropout_p": 0.1,
+                "num_layers": 6,
+                "num_heads": 2,
+                "hidden_channels_ffn": 768,
+                "input_length": None,
+            }
+        elif encoder == "gated":
+            encoder_type = "gated_conv"
+            encoder_params = {
+                "kernel_size": 5,
+                "dropout_p": 0.1,
+                "num_layers": 9,
+            }
+        elif encoder == "residual_bn":
+            encoder_type = "residual_conv_bn"
+            encoder_params = {
+                "kernel_size": 4,
+                "dilations": [1, 2, 4, 1, 2, 4, 1, 2, 4, 1, 2, 4, 1],
+                "num_conv_blocks": 2,
+                "num_res_blocks": 13,
+            }
+        elif encoder == "time_depth":
+            encoder_type = "time_depth_separable"
+            encoder_params = {
+                "kernel_size": 5,
+                "num_layers": 9,
+            }
+        config = GlowTTSConfig(
+            audio=audio,
+            run_name="multispeaker glow tts",
+            run_description="glow tts for multispeaker datasets",
+            batch_size=self.batch_size,
+            eval_batch_size=self.batch_size // 2,
+            mixed_precision=self.mixed_precision,
+            run_eval=True,
+            test_delay_epochs=-1,
+            print_eval=False,
+            print_step=25,
+            tb_plot_step=100,
+            tb_model_param_stats=False,
+            save_step=10000,
+            num_loader_workers=8,
+            num_eval_loader_workers=8,
+            use_noise_augment=False,
+            output_path=self.output_path,
+            use_phonemes=True,
+            use_espeak_phonemes=True,
+            phoneme_language="en",
+            compute_input_seq_cache=False,
+            test_sentences_file=None,
+            phoneme_cache_path=self.phoneme_path,
+            batch_group_size=0,
+            loss_masking=True,
+            min_seq_len=2,
+            max_seq_len=500,
+            compute_f0=False,
+            add_blank=True,
+            use_speaker_embedding=True,
+            use_d_vector_file=True,
+            d_vector_dim=256,
+            encoder_type=encoder_type,
+            encoder_params=encoder_params,
+            use_encoder_prenet=True,
+            hidden_channels_dp=256,
+            hidden_channels_dec=192,
+            hidden_channels_enc=192,
+            dropout_p_dp=0.1,
+            dropout_p_dec=0.05,
+            mean_only=True,
+            out_channels=80,
+            num_flow_blocks_dec=12,
+            inference_noise_scale=0.0,
+            kernel_size_dec=5,
+            dilation_rate=1,
+            num_block_layers=4,
+            num_speakers=0,
+            num_splits=4,
+            num_squeeze=2,
+            sigmoid_scale=False,
+            data_dep_init_steps=10,
+            style_wav_for_test=None,
+            length_scale=1.0,
+            d_vector_file=speaker_file,
+            grad_clip=5.0,
+            lr=self.learning_rate,
+            r=1,
+            datasets=[dataset],
+        )
+        return config
