88450c4c18e798a82a3b27ddbe6eda190b70f7c9
Eren Gölge <egolge@coqui.ai>
2021-08-18 14:55:46 +0000

    Fix VITS model SPD

--- TTS/tts/configs/vits_config.py
+++ TTS/tts/configs/vits_config.py
@@ -119,8 +119,8 @@ class VitsConfig(BaseTTSConfig):
     compute_linear_spec: bool = True
 
     # overrides
-    min_seq_len: int = 13
-    max_seq_len: int = 500
+    min_seq_len: int = 32
+    max_seq_len: int = 1000
     r: int = 1  # DO NOT CHANGE
     add_blank: bool = True
 
