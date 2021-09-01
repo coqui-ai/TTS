789916db7a111a848f9b7c45a9c6d371180c679b
Katsuya Iida <katsuya.iida@gmail.com>
2021-09-01 16:33:15 +0900

    Update Japanese phonemizer  (#758)
    * Update default ja vocoder
    
    * update
    
    * Japanese phonemizer test
    
    * Run make style
    
    Co-authored-by: Eren Gölge <egolge@coqui.ai>

--- tests/text_tests/test_japanese_phonemizer.py
+++ tests/text_tests/test_japanese_phonemizer.py
@@ -5,11 +5,13 @@ from TTS.tts.utils.text.japanese.phonemizer import japanese_text_to_phonemes
 _TEST_CASES = """
 どちらに行きますか？/dochiraniikimasuka?
 今日は温泉に、行きます。/kyo:waoNseNni,ikimasu.
-「A」から「Z」までです。/AkaraZmadedesu.
+「A」から「Z」までです。/e:karazeqtomadedesu.
 そうですね！/so:desune!
 クジラは哺乳類です。/kujirawahonyu:ruidesu.
 ヴィディオを見ます。/bidioomimasu.
-ky o: w a o N s e N n i , i k i m a s u ./kyo:waoNseNni,ikimasu.
+今日は８月22日です/kyo:wahachigatsuniju:ninichidesu
+xyzとαβγ/eqkusuwaizeqtotoarufabe:tagaNma
+値段は$12.34です/nedaNwaju:niteNsaNyoNdorudesu
 """
 
 
