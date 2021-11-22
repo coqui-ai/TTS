a358c561cdc596f0e5f9087a9885cf0e7e022c3f
Edresson <edresson1@gmail.com>
2021-11-22 13:43:44 -0300

    Fix the bug in extract tts spectrograms

--- TTS/bin/train_tts.py
+++ TTS/bin/train_tts.py
@@ -64,7 +64,7 @@ def main():
         train_samples=train_samples,
         eval_samples=eval_samples,
         training_assets={"audio_processor": ap},
-        parse_command_line_args=True,
+        parse_command_line_args=False,
     )
     trainer.fit()
 
