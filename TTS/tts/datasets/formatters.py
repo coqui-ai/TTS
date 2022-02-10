d2249ed435b7ebd59f3077779efab21cdf13ee70
arif334 <arif334@users.noreply.github.com>
2022-02-10 15:58:24 +0600

    updated encoding for ljspeech, and added strip() to wav name
    `utf-8-sig` is proposed to avoid Byte Order Mark (BOM). 
    Adding a strip() function when generating wav_file names is necessary if the text contains spaces around "|".

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -114,10 +114,10 @@ def ljspeech(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
     txt_file = os.path.join(root_path, meta_file)
     items = []
     speaker_name = "ljspeech"
-    with open(txt_file, "r", encoding="utf-8") as ttf:
+    with open(txt_file, "r", encoding="utf-8-sig") as ttf:
         for line in ttf:
             cols = line.split("|")
-            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+            wav_file = os.path.join(root_path, "wavs", cols[0].strip() + ".wav")
             text = cols[2]
             items.append([text, wav_file, speaker_name])
     return items
@@ -128,10 +128,10 @@ def ljspeech_test(root_path, meta_file, **kwargs):  # pylint: disable=unused-arg
     https://keithito.com/LJ-Speech-Dataset/"""
     txt_file = os.path.join(root_path, meta_file)
     items = []
-    with open(txt_file, "r", encoding="utf-8") as ttf:
+    with open(txt_file, "r", encoding="utf-8-sig") as ttf:
         for idx, line in enumerate(ttf):
             cols = line.split("|")
-            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+            wav_file = os.path.join(root_path, "wavs", cols[0].strip() + ".wav")
             text = cols[2]
             items.append([text, wav_file, f"ljspeech-{idx}"])
     return items
