95c524e9e3c1bf515d870dd653cae51d04112a1e
Lucas Hideki Ueda <lucashueda@gmail.com>
2022-03-23 23:29:09 -0300

    allowing command line params

--- TTS/bin/train_tts.py
+++ TTS/bin/train_tts.py
@@ -95,7 +95,7 @@ def main():
         train_samples=train_samples,
         eval_samples=eval_samples,
         training_assets={"audio_processor": ap},
-        parse_command_line_args=False,
+        parse_command_line_args=True, #Change to allow control via command line
     )
     trainer.fit()
 
