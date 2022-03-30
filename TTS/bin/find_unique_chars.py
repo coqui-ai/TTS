012dc110d1101aee21fe09648c635dd7bd1b238d
p0p4k <rajiv.punmiya@gmail.com>
2022-03-30 13:04:51 +0900

    Update find_unique_chars.py

--- TTS/bin/find_unique_chars.py
+++ TTS/bin/find_unique_chars.py
@@ -29,7 +29,7 @@ def main():
 
     items = train_items + eval_items
 
-    texts = "".join(item[0] for item in items)
+    texts = "".join(item['text'] for item in items)
     chars = set(texts)
     lower_chars = filter(lambda c: c.islower(), chars)
     chars_force_lower = [c.lower() for c in chars]
