ea04aa7b306b7d3dd2e8e3dce40b4f3086a664d0
WeberJulian <julian.weber@hotmail.fr>
2022-04-13 00:23:19 +0200

    fix: remove unused import

--- tests/tts_tests/test_tacotron2_model.py
+++ tests/tts_tests/test_tacotron2_model.py
@@ -8,7 +8,7 @@ from torch import nn, optim
 from tests import get_tests_input_path
 from TTS.tts.configs.shared_configs import CapacitronVAEConfig, GSTConfig
 from TTS.tts.configs.tacotron2_config import Tacotron2Config
-from TTS.tts.layers.losses import MSELossMasked, TacotronLoss
+from TTS.tts.layers.losses import MSELossMasked
 from TTS.tts.models.tacotron2 import Tacotron2
 from TTS.utils.audio import AudioProcessor
 
