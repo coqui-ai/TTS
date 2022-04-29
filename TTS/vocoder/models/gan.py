5e27ac7f56fbf949698a7055a27df4bfd9d44688
Edresson Casanova <edresson1@gmail.com>
2022-04-29 07:28:39 -0300

    Fix style

--- TTS/vocoder/models/gan.py
+++ TTS/vocoder/models/gan.py
@@ -89,7 +89,6 @@ class GAN(BaseVocoder):
         if optimizer_idx not in [0, 1]:
             raise ValueError(" [!] Unexpected `optimizer_idx`.")
 
-        
         if optimizer_idx == 0:
             # DISCRIMINATOR optimization
 
