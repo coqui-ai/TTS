fd5ab8f1bcaf78adac209500984ffadd4c72eac4
Eren Gölge <egolge@coqui.ai>
2022-05-09 19:42:39 +0200

    Drop 🐍p3.6 support
    Numpy also dropped support for p3.6

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ import setuptools.command.develop
 from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
-if LooseVersion(sys.version) < LooseVersion("3.6") or LooseVersion(sys.version) >= LooseVersion("3.11"):
-    raise RuntimeError("TTS requires python > 3.5 and < 3.11 " "but your Python version is {}".format(sys.version))
+if LooseVersion(sys.version) < LooseVersion("3.7") or LooseVersion(sys.version) >= LooseVersion("3.11"):
+    raise RuntimeError("TTS requires python >= 3.7 and < 3.11 " "but your Python version is {}".format(sys.version))
 
 
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -113,12 +113,11 @@ setup(
         "dev": requirements_dev,
         "notebooks": requirements_notebooks,
     },
-    python_requires=">=3.6.0, <3.11",
+    python_requires=">=3.7.0, <3.11",
     entry_points={"console_scripts": ["tts=TTS.bin.synthesize:main", "tts-server = TTS.server.server:main"]},
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
