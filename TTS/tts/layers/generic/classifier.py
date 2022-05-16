024e56784976308ebd0bc24c059348645082ba72
Edresson Casanova <edresson1@gmail.com>
2022-05-16 13:09:12 +0000

    Clean up old code

--- TTS/tts/layers/generic/classifier.py
+++ TTS/tts/layers/generic/classifier.py
@@ -1,8 +1,10 @@
 import torch
 from torch import nn
 
+
+# pylint: disable=W0223
 class GradientReversalFunction(torch.autograd.Function):
-    """Revert gradient without any further input modification. 
+    """Revert gradient without any further input modification.
     Adapted from: https://github.com/Tomiinek/Multilingual_Text_to_Speech/"""
 
     @staticmethod
@@ -30,17 +32,16 @@ class ReversalClassifier(nn.Module):
     """
 
     def __init__(self, in_channels, out_channels, hidden_channels, gradient_clipping_bounds=0.25, scale_factor=1.0):
-        super(ReversalClassifier, self).__init__()
+        super().__init__()
         self._lambda = scale_factor
         self._clipping = gradient_clipping_bounds
         self._out_channels = out_channels
         self._classifier = nn.Sequential(
-            nn.Linear(in_channels, hidden_channels),
-            nn.ReLU(),
-            nn.Linear(hidden_channels, out_channels)
+            nn.Linear(in_channels, hidden_channels), nn.ReLU(), nn.Linear(hidden_channels, out_channels)
         )
         self.test = nn.Linear(in_channels, hidden_channels)
-    def forward(self, x, labels, x_mask=None):  
+
+    def forward(self, x, labels, x_mask=None):
         x = GradientReversalFunction.apply(x, self._lambda, self._clipping)
         x = self._classifier(x)
         loss = self.loss(labels, x, x_mask)
@@ -55,7 +56,7 @@ class ReversalClassifier(nn.Module):
         ml = torch.max(x_mask)
         input_mask = torch.arange(ml, device=predictions.device)[None, :] < x_mask[:, None]
 
-        target = labels.repeat(ml.int().item(), 1).transpose(0,1)
+        target = labels.repeat(ml.int().item(), 1).transpose(0, 1)
         target[~input_mask] = ignore_index
 
-        return nn.functional.cross_entropy(predictions.transpose(1,2), target, ignore_index=ignore_index)
+        return nn.functional.cross_entropy(predictions.transpose(1, 2), target, ignore_index=ignore_index)
