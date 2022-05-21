53ea88b5bf2d1e52003ab21fc239afb6888328eb
WeberJulian <julian.weber@hotmail.fr>
2022-05-21 22:37:48 +0200

    Change default optimizer params

--- TTS/enhancer/config/hifigan_config.py
+++ TTS/enhancer/config/hifigan_config.py
@@ -11,14 +11,14 @@ class HifiganConfig(BaseEnhancerConfig):
     model_args: HifiGANArgs = field(default_factory=HifiGANArgs)
     target_sr: int = 16000
     input_sr: int = 16000
-    segment_len: float = 1.5
-    steps_to_start_discriminator: int = 100_000
+    segment_len: float = 2
+    steps_to_start_discriminator: int = 40_000
     steps_to_start_postnet: int = 20_000
     gt_augment: bool = True
     lr_gen: float = 0.002
-    lr_disc: float = 0.02
+    lr_disc: float = 0.002
     lr_scheduler_gen: str = "StepLR"
-    lr_scheduler_gen_params: dict = field(default_factory=lambda: {"step_size": 30, "gamma": 0.1})
+    lr_scheduler_gen_params: dict = field(default_factory=lambda: {})
     lr_scheduler_disc: str = "StepLR"
-    lr_scheduler_disc_params: dict = field(default_factory=lambda: {"step_size": 30, "gamma": 0.1})
+    lr_scheduler_disc_params: dict = field(default_factory=lambda: {})
 
