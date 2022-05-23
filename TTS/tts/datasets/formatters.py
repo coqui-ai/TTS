5857941043c6437d7a62e75cc20909bfdd6572b7
WeberJulian <julian.weber@hotmail.fr>
2022-05-23 18:43:47 +0200

    Add synpaflex formatter

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -366,6 +366,20 @@ def vctk_old(root_path, meta_files=None, wavs_path="wav48", ignored_speakers=Non
         items.append({"text": text, "audio_file": wav_file, "speaker_name": "VCTK_old_" + speaker_id})
     return items
 
+def synpaflex(root_path, meta_files=None, wavs_path=None, ignored_speakers=None):
+    items = []
+    speaker_name = "synpaflex"
+    root_path = os.path.join(root_path, "")
+    wav_files = glob(f"{root_path}**/*.wav", recursive=True)
+    for wav_file in wav_files:
+        if os.sep + "wav" + os.sep in wav_file:
+            txt_file = wav_file.replace("wav", "txt")
+        else:
+            txt_file = os.path.join(os.path.dirname(wav_file), "txt", os.path.basename(wav_file).replace(".wav", ".txt"))
+        with open(txt_file, "r", encoding="utf-8") as file_text:
+            text = file_text.readlines()[0]
+        items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name})
+    return items
 
 def open_bible(root_path, meta_files="train", ignore_digits_sentences=True, ignored_speakers=None):
     """ToDo: Refer the paper when available"""
