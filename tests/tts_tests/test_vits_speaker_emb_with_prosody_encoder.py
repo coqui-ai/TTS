f774cf0648857c774ef6ac2f51425c40158d29ca
Edresson Casanova <edresson1@gmail.com>
2022-05-26 15:41:24 -0300

    Condition the prosody encoder on z_p

--- tests/tts_tests/test_vits_speaker_emb_with_prosody_encoder.py
+++ tests/tts_tests/test_vits_speaker_emb_with_prosody_encoder.py
@@ -45,8 +45,9 @@ config.model_args.use_prosody_encoder = True
 config.model_args.prosody_embedding_dim = 64
 # active classifier
 config.model_args.external_emotions_embs_file = "tests/data/ljspeech/speakers.json"
-config.model_args.use_prosody_enc_emo_classifier = True
+config.model_args.use_prosody_enc_emo_classifier = False
 config.model_args.use_text_enc_emo_classifier = True
+config.model_args.use_prosody_encoder_z_p_input = True
 
 config.save_json(config_path)
 
