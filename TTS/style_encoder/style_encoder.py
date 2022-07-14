43f3da3dbb446c786a3c41505bf5b11d6169d222
Lucas Hideki Ueda <lucashueda@gmail.com>
2022-07-14 14:31:47 -0300

    adding verbose in adain debug

--- TTS/style_encoder/style_encoder.py
+++ TTS/style_encoder/style_encoder.py
@@ -293,13 +293,19 @@ class StyleEncoder(nn.Module):
         mean_content = torch.mean(outputs, dim= [-2])
         std_content = torch.std(outputs, dim= [-2]) + 1e-5
 
-        # embedded_speakers_ = embedded_speakers.expand(outputs.size(0), outputs.size(1), -1)
+        embedded_speakers_ = embedded_speakers.expand(outputs.size(0), outputs.size(1), -1)
         
+        print('embed_speakers shape : ' , embedded_speakers_.shape)
+
         mean_style = torch.mean(embedded_speakers, dim= [-2])
         std_style = torch.std(embedded_speakers, dim= [-2]) + 1e-5
 
+        print(mean_style.shape, std_style.shape, mean_style, std_style)
+
         mean_style = mean_style.expand(outputs.size(0), outputs.size(1), -1)
         std_style = std_style.expand(outputs.size(0), outputs.size(1), -1)
+
+        print(mean_style.shape, std_style.shape, mean_style, std_style)
 
         # add verbose to debug nan errors: the hypothesis is that the mean becomes high
         # print(mean_content, std_content, mean_style, std_style) # Apparently the error was dividing by 0, added 1e-5
