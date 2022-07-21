32fcdd734af73064446833f7378bd75663ae047a
46319943 <495384481@qq.com>
2022-07-21 20:30:59 +0800

    Reformat.

--- TTS/tts/utils/text/chinese_mandarin/phonemizer.py
+++ TTS/tts/utils/text/chinese_mandarin/phonemizer.py
@@ -6,9 +6,14 @@ from .pinyinToPhonemes import PINYIN_DICT
 
 
 def _chinese_character_to_pinyin(text: str) -> List[str]:
-    pinyins = pypinyin.pinyin(text, style=pypinyin.Style.TONE3, heteronym=False, neutral_tone_with_five=True,
-                              # Add temp non-pinyin flag
-                              errors=lambda x: '￥' + x)
+    pinyins = pypinyin.pinyin(
+        text,
+        style=pypinyin.Style.TONE3,
+        heteronym=False,
+        neutral_tone_with_five=True,
+        # Add temp non-pinyin flag
+        errors=lambda x: "￥" + x,
+    )
     pinyins_flat_list = [item for sublist in pinyins for item in sublist]
     return pinyins_flat_list
 
@@ -21,7 +26,7 @@ def _chinese_pinyin_to_phoneme(pinyin: str) -> str:
 
 
 def is_pinyin(token: str):
-    return not token.startswith('￥')
+    return not token.startswith("￥")
 
 
 def chinese_text_to_phonemes(text: str, seperator: str = "|") -> str:
@@ -36,6 +41,6 @@ def chinese_text_to_phonemes(text: str, seperator: str = "|") -> str:
         else:  # is ponctuation or other
             results += list(token[1:])  # remove the temp flag
 
-        results.append(' ')
+        results.append(" ")
 
     return seperator.join(results).strip()
