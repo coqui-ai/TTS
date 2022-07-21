5dca04a82ebc5f333561142a342b5332ea906575
Lucas Hideki Ueda <lucashueda@gmail.com>
2022-07-21 19:42:48 -0300

    unsqueezing lookup emb

--- TTS/tts/models/styleforward_tts.py
+++ TTS/tts/models/styleforward_tts.py
@@ -571,8 +571,10 @@ class StyleforwardTTS(BaseTTS):
 
         #Style embedding 
         if(self.config.style_encoder_config.use_lookup):
-            print(self.emb_s(aux_input["style_ids"]).unsqueeze(-1).shape, o_en.shape)
-            o_en = o_en + self.emb_s(aux_input["style_ids"].unsqueeze(-1)) # [B, E , 1]
+            # print(self.emb_s(aux_input["style_ids"]).unsqueeze(1).shape, o_en.shape)
+            o_en = o_en.permute(0,2,1)
+            o_en = o_en + self.emb_s(aux_input["style_ids"].unsqueeze(1)) # [B, 1, C]
+            o_en = o_en.permute(0,2,1)
         else:
             se_inputs = [o_en.permute(0,2,1), y]
             o_en, style_encoder_outputs = self.style_encoder_layer.forward(se_inputs, aux_input["style_ids"])
@@ -651,7 +653,9 @@ class StyleforwardTTS(BaseTTS):
         # o_en, style_encoder_outputs = self.style_encoder_layer.forward(se_inputs)
         
         if(self.config.style_encoder_config.use_lookup):
-            o_en = o_en + self.emb_s(aux_input["style_ids"].unsqueeze(-1)) # [B, E , 1]
+            o_en = o_en.permute(0,2,1)
+            o_en = o_en + self.emb_s(aux_input["style_ids"].unsqueeze(1)) # [B, 1, C]
+            o_en = o_en.permute(0,2,1)
         else:
             se_inputs = [o_en.permute(0,2,1), aux_input['style_mel']]
             o_en, style_encoder_outputs = self.style_encoder_layer.forward(se_inputs)
