5cfe89a9dd2c59b664e2fce37500cdba2fe37f2a
Lucas Hideki Ueda <lucashueda@gmail.com>
2022-07-28 16:29:25 -0300

    verbose debug

--- TTS/tts/layers/losses.py
+++ TTS/tts/layers/losses.py
@@ -1088,7 +1088,7 @@ class StyleForwardTTSLoss(nn.Module):
                 loss += speaker_dot_loss
                 return_dict["speaker_orthogonal_loss"] = speaker_dot_loss
             if self.style_encoder_config.use_guided_style:
-                # print(style_preds.shape, style_ids.shape)
+                print(style_preds.shape, style_ids.shape)
                 style_guided_loss = self.criterion_guided(style_preds.squeeze(1), style_ids) # Must squeeze cuz it was augmented for broadcasting
                 loss += style_guided_loss
                 return_dict["style_guided_loss"] = style_guided_loss
