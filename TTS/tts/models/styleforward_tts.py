211a157d33cc471060faf68449f6bb6f78a28cc8
Lucas Hideki Ueda <lucashueda@gmail.com>
2022-07-29 01:10:31 -0300

    adding ce weights and fixing inference

--- TTS/tts/models/styleforward_tts.py
+++ TTS/tts/models/styleforward_tts.py
@@ -674,7 +674,7 @@ class StyleforwardTTS(BaseTTS):
             o_en = o_en.permute(0,2,1)
         else:
             se_inputs = [o_en.permute(0,2,1), aux_input['style_mel']]
-            o_en, style_encoder_outputs = self.style_encoder_layer.forward(se_inputs)
+            o_en, style_encoder_outputs = self.style_encoder_layer.forward(se_inputs, aux_input["style_ids"])
             o_en = o_en.permute(0,2,1)
 
 
