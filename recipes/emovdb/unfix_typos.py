ec949996cebe0ae54d2190de8a81cd1e2b22fbb7
LeonardoBoulitreau <leonardoboulitreau@gmail.com>
2022-09-09 17:59:12 -0300

    prepare neutral emovdb

---
+++ recipes/emovdb/unfix_typos.py
@@ -0,0 +1,7 @@
+import os
+
+os.rename('files/josh_Amused/amused_197-224_0223.wav', 'files/josh_Amused/amused_197-224_0023.wav')
+os.rename('files/josh_Amused/amused_225_252_0226.wav', 'files/josh_Amused/amused_225_252_0026.wav')
+os.rename('files/bea_Neutral/neutral_113-140_0114.wav', 'files/bea_Neutral/neutral_113-140_0014.wav')
+os.rename('files/bea_Neutral/neutral_113-140_0115.wav', 'files/bea_Neutral/neutral_113-140_0015.wav')
+os.rename('files/bea_Angry/anger_197-224_0224.wav', 'files/bea_Angry/anger_197-224_0024.wav')
