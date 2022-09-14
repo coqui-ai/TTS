2334aa69b612d34a6b1f934ff9d62338b8905ee3
junyongleeme <leejunyong1150@gmail.com>
2022-09-14 11:13:08 +0900

    Issue resolution in which g2p is called even though Korean_phoneemizer is not used due to the global variable g2p object

--- TTS/tts/utils/text/korean/phonemizer.py
+++ TTS/tts/utils/text/korean/phonemizer.py
@@ -3,7 +3,7 @@ from jamo import hangul_to_jamo
 
 from TTS.tts.utils.text.korean.korean import normalize
 
-g2p = G2p()
+g2p = None
 
 
 def korean_text_to_phonemes(text, character: str = "hangeul") -> str:
@@ -17,6 +17,11 @@ def korean_text_to_phonemes(text, character: str = "hangeul") -> str:
         output = '하늘' (Unicode :\u1112\u1161\u1102\u1173\u11af), (ᄒ + ᅡ + ᄂ + ᅳ + ᆯ)
 
     """
+
+    global g2p
+
+    if g2p is None:
+        g2p = G2p()
 
     if character == "english":
         from anyascii import anyascii
