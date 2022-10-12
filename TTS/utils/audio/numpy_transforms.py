30ebf130c9b1414c38c3ebf4e64df7eb6374afe0
manmay nakhashi <manmay.nakhashi@gmail.com>
2022-10-12 12:57:23 +0530

    Update numpy_transforms.py

--- TTS/utils/audio/numpy_transforms.py
+++ TTS/utils/audio/numpy_transforms.py
@@ -303,25 +303,24 @@ def compute_f0(
     return f0
 
 def compute_energy(y: np.ndarray, **kwargs) -> np.ndarray:
-   """Compute energy of a waveform using the same parameters used for computing melspectrogram.
-      Args:
-        x (np.ndarray): Waveform. Shape :math:`[T_wav,]`
-      Returns:
-        np.ndarray: energy. Shape :math:`[T_energy,]`. :math:`T_energy == T_wav / hop_length`
-      Examples:
-        >>> WAV_FILE = filename = librosa.util.example_audio_file()
-        >>> from TTS.config import BaseAudioConfig
-        >>> from TTS.utils.audio import AudioProcessor
-        >>> conf = BaseAudioConfig()
-        >>> ap = AudioProcessor(**conf)
-        >>> wav = ap.load_wav(WAV_FILE, sr=ap.sample_rate)[:5 * ap.sample_rate]
-        >>> energy = ap.compute_energy(wav)
-   """
-   x = stft(y, **kwargs)
-   mag , _ = magphase(x)
-   energy = np.sqrt(np.sum(mag ** 2, axis=0))
-   return energy
-      
+    """Compute energy of a waveform using the same parameters used for computing melspectrogram.
+       Args:
+         x (np.ndarray): Waveform. Shape :math:`[T_wav,]`
+       Returns:
+         np.ndarray: energy. Shape :math:`[T_energy,]`. :math:`T_energy == T_wav / hop_length`
+       Examples:
+         >>> WAV_FILE = filename = librosa.util.example_audio_file()
+         >>> from TTS.config import BaseAudioConfig
+         >>> from TTS.utils.audio import AudioProcessor
+         >>> conf = BaseAudioConfig()
+         >>> ap = AudioProcessor(**conf)
+         >>> wav = ap.load_wav(WAV_FILE, sr=ap.sample_rate)[:5 * ap.sample_rate]
+         >>> energy = ap.compute_energy(wav)
+    """
+    x = stft(y=y, **kwargs)
+    mag , _ = magphase(x)
+    energy = np.sqrt(np.sum(mag ** 2, axis=0))
+    return energy
 ### Audio Processing ###
 def find_endpoint(
     *,
