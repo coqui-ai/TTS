e14f0e661d218dd9af45e1a8c0f88468f6724dbe
Koichiro Mori <f2forest@gmail.com>
2022-11-18 10:38:24 +0900

    Add jsut formatter

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -601,3 +601,19 @@ def kss(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
             text = cols[2]  # cols[1] => 6월, cols[2] => 유월
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name})
     return items
+
+
+def jsut(root_path, meta_file, **kwargs):
+    """JSUT (Japanese speech corpus of Saruwatari-lab., University of Tokyo)
+    https://sites.google.com/site/shinnosuketakamichi/publication/jsut"""
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "jsut"
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            line = line.rstrip()
+            cols = line.split(":")
+            wav_file = os.path.join(root_path, "wav", cols[0] + ".wav")
+            text = cols[1]
+            items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
+    return items
