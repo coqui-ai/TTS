783a9825ca28ea36ffac85aba0370ae5f0460cbe
Shivam Mehta <shivam.mehta007@gmail.com>
2022-12-01 14:19:31 +0000

    Removing runs

--- recipes/ljspeech/tacotron2-DDC/run-November-24-2022_01+19PM-c50d89fc/train_tacotron_ddc.py
+++ recipes/ljspeech/tacotron2-DDC/run-November-24-2022_01+19PM-c50d89fc/train_tacotron_ddc.py
@@ -3,10 +3,10 @@ import os
 from trainer import Trainer, TrainerArgs
 
 from TTS.config.shared_configs import BaseAudioConfig
-from TTS.tts.configs.overflow_config import OverFlowConfig
 from TTS.tts.configs.shared_configs import BaseDatasetConfig
+from TTS.tts.configs.tacotron2_config import Tacotron2Config
 from TTS.tts.datasets import load_tts_samples
-from TTS.tts.models.overflow import OverFlow
+from TTS.tts.models.tacotron2 import Tacotron2
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
 
@@ -32,25 +32,26 @@ audio_config = BaseAudioConfig(
     preemphasis=0.0,
 )
 
-config = OverFlowConfig(  # This is the config that is saved for the future use
+config = Tacotron2Config(  # This is the config that is saved for the future use
     audio=audio_config,
-    batch_size=32,
+    batch_size=64,
     eval_batch_size=16,
     num_loader_workers=4,
     num_eval_loader_workers=4,
     run_eval=True,
     test_delay_epochs=-1,
+    r=6,
+    gradual_training=[[0, 6, 64], [10000, 4, 32], [50000, 3, 32], [100000, 2, 32]],
+    double_decoder_consistency=True,
     epochs=1000,
     text_cleaner="phoneme_cleaners",
     use_phonemes=True,
     phoneme_language="en-us",
     phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
     precompute_num_workers=8,
-    normalize_mel=True,
-    normalized_mel_parameter_path = os.path.join(output_path, "normalized_mel_parameters.pt"),
     print_step=25,
     print_eval=True,
-    mixed_precision=True,
+    mixed_precision=False,
     output_path=output_path,
     datasets=[dataset_config],
 )
@@ -84,7 +85,7 @@ train_samples, eval_samples = load_tts_samples(
 # Models take a config object and a speaker manager as input
 # Config defines the details of the model like the number of layers, the size of the embedding, etc.
 # Speaker manager is used by multi-speaker models.
-model = OverFlow(config, ap, tokenizer)
+model = Tacotron2(config, ap, tokenizer, speaker_manager=None)
 
 # init the trainer and 🚀
 trainer = Trainer(
