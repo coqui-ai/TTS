2cabe3f84deb1beb328ccc59deb497f4243143f0
Eren G??lge <egolge@coqui.ai>
2022-12-08 10:42:37 +0100

    Fix style

--- TTS/api.py
+++ TTS/api.py
@@ -1,12 +1,13 @@
 from pathlib import Path
-from TTS.utils.synthesizer import Synthesizer
+
 from TTS.utils.manage import ModelManager
+from TTS.utils.synthesizer import Synthesizer
 
 
-class TTS():
-    """TODO: Add voice conversion and Capacitron support.
-    """
-    def __init__(self, model_name:str=None, progress_bar:bool=True, gpu=False):
+class TTS:
+    """TODO: Add voice conversion and Capacitron support."""
+
+    def __init__(self, model_name: str = None, progress_bar: bool = True, gpu=False):
         """🐸TTS python interface that allows to load and use the released models.
 
         Args:
@@ -50,30 +51,30 @@ class TTS():
     def get_models_file_path(self):
         return Path(__file__).parent / ".models.json"
 
-    def download_model_by_name(self, model_name:str):
+    def download_model_by_name(self, model_name: str):
         model_path, config_path, model_item = self.manager.download_model(model_name)
         if model_item["default_vocoder"] is None:
             return model_path, config_path, None, None
         vocoder_path, vocoder_config_path, _ = self.manager.download_model(model_item["default_vocoder"])
         return model_path, config_path, vocoder_path, vocoder_config_path
 
-    def load_model_by_name(self, model_name:str, gpu:bool=False):
+    def load_model_by_name(self, model_name: str, gpu: bool = False):
         model_path, config_path, vocoder_path, vocoder_config_path = self.download_model_by_name(model_name)
         # init synthesizer
         # None values are fetch from the model
         self.synthesizer = Synthesizer(
-                                    tts_checkpoint = model_path,
-                                    tts_config_path = config_path,
-                                    tts_speakers_file = None,
-                                    tts_languages_file= None,
-                                    vocoder_checkpoint=vocoder_path,
-                                    vocoder_config=vocoder_config_path,
-                                    encoder_checkpoint = None,
-                                    encoder_config=None,
-                                    use_cuda=gpu,
-                                )
+            tts_checkpoint=model_path,
+            tts_config_path=config_path,
+            tts_speakers_file=None,
+            tts_languages_file=None,
+            vocoder_checkpoint=vocoder_path,
+            vocoder_config=vocoder_config_path,
+            encoder_checkpoint=None,
+            encoder_config=None,
+            use_cuda=gpu,
+        )
 
-    def _check_arguments(self, text:str, speaker:str=None, language:str=None):
+    def _check_arguments(self, text: str, speaker: str = None, language: str = None):
         if self.is_multi_speaker and speaker is None:
             raise ValueError("Model is multi-speaker but no speaker is provided.")
         if self.is_multi_lingual and language is None:
@@ -83,7 +84,7 @@ class TTS():
         if not self.is_multi_lingual and language is not None:
             raise ValueError("Model is not multi-lingual but language is provided.")
 
-    def tts(self, text:str, speaker:str=None, language:str=None):
+    def tts(self, text: str, speaker: str = None, language: str = None):
         """Convert text to speech.
 
         Args:
@@ -99,9 +100,9 @@ class TTS():
         self._check_arguments(text=text, speaker=speaker, language=language)
 
         wav = self.synthesizer.tts(
-            text = text,
-            speaker_name= speaker,
-            language_name = language,
+            text=text,
+            speaker_name=speaker,
+            language_name=language,
             speaker_wav=None,
             reference_wav=None,
             style_wav=None,
@@ -110,7 +111,7 @@ class TTS():
         )
         return wav
 
-    def tts_to_file(self, text:str, speaker:str=None, language:str=None, output_file_path:str="output.wav"):
+    def tts_to_file(self, text: str, speaker: str = None, language: str = None, output_file_path: str = "output.wav"):
         """Convert text to speech.
 
         Args:
@@ -129,7 +130,6 @@ class TTS():
         self.synthesizer.save_wav(wav=wav, path=output_file_path)
 
 
-
 if __name__ == "__main__":
     tts = TTS()
     print(tts.models[0])
@@ -142,7 +142,6 @@ if __name__ == "__main__":
     # print(tts.languages)
 
     # wav = tts.tts_to_file(text="Hello world!", speaker=tts.speakers[0], language=tts.languages[0])
-
 
     # tts.load_model_by_name(tts.models[1])
     # print(tts.is_multi_speaker)
