c2df9f3949836dda225ec4b4965b3203e66de052
Shivam Mehta <shivam.mehta007@gmail.com>
2022-12-10 09:39:23 +0000

    Adding information about parameters and docstring

--- TTS/tts/models/overflow.py
+++ TTS/tts/models/overflow.py
@@ -43,6 +43,19 @@ class Overflow(BaseTTS):
     whilst retaining the original advantages of neural HMMs. Audio examples and code
     are available at https://shivammehta25.github.io/OverFlow/.
 
+    Note:
+        - Neural HMMs uses flat start initialization i.e it computes the means and std and transition probabilities
+        of the dataset and uses them to initialize the model. This benefits the model and helps with faster learning
+        If you change the dataset or want to regenerate the parameters change the `force_generate_statistics` and
+        `mel_statistics_parameter_path` accordingly.
+
+        - To enable multi-GPU training, set the `use_grad_checkpointing=False` in config.
+        This will significantly increase the memory usage.  This is because to compute
+        the actual data likelihood (not an approximation using MAS/Viterbi) we must use
+        all the states at the previous time step during the forward pass to decide the
+        probability distribution at the current step i.e the difference between the forward
+        algorithm and viterbi approximation.
+
     Check :class:`TTS.tts.configs.overflow.OverFlowConfig` for class arguments.
     """
 
