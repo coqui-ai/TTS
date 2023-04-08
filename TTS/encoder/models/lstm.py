db9fb51de86e03b4d4ce39da058458ac79d6aca3
Daniel Gonzálbez Biosca <daniel.gonzalbez@veue02.tsc.upc.edu>
2023-04-08 10:02:23 +0200

    Added function for computing embeddings

--- TTS/encoder/models/lstm.py
+++ TTS/encoder/models/lstm.py
@@ -39,15 +39,11 @@ class LSTMSpeakerEncoder(BaseEncoder):
         lstm_dim=768,
         num_lstm_layers=3,
         use_lstm_with_projection=True,
-        use_torch_spec=False,
-        audio_config=None,
+        audio_config=None
     ):
         super().__init__()
         self.use_lstm_with_projection = use_lstm_with_projection
-        self.use_torch_spec = use_torch_spec
-        self.audio_config = audio_config
         self.proj_dim = proj_dim
-
         layers = []
         # choise LSTM layer
         if use_lstm_with_projection:
@@ -57,13 +53,6 @@ class LSTMSpeakerEncoder(BaseEncoder):
             self.layers = nn.Sequential(*layers)
         else:
             self.layers = LSTMWithoutProjection(input_dim, lstm_dim, proj_dim, num_lstm_layers)
-
-        self.instancenorm = nn.InstanceNorm1d(input_dim)
-
-        if self.use_torch_spec:
-            self.torch_spec = self.get_torch_mel_spectrogram_class(audio_config)
-        else:
-            self.torch_spec = None
 
         self._init_layers()
 
@@ -85,15 +74,54 @@ class LSTMSpeakerEncoder(BaseEncoder):
         Shapes:
             - x: :math:`(N, 1, T_{in})` or :math:`(N, D_{spec}, T_{in})`
         """
-        with torch.no_grad():
-            with torch.cuda.amp.autocast(enabled=False):
-                if self.use_torch_spec:
-                    x.squeeze_(1)
-                    x = self.torch_spec(x)
-                x = self.instancenorm(x).transpose(1, 2)
         d = self.layers(x)
         if self.use_lstm_with_projection:
-            d = d[:, -1]
+            d = torch.nn.functional.normalize(d[:, -1], p=2, dim=1)
         if l2_norm:
             d = torch.nn.functional.normalize(d, p=2, dim=1)
         return d
+
+
+    def compute_speaker_reference(self, x, num_frames=160, overlap=0.5):
+        """
+        Generate embeddings for a batch of utterances
+        x: 1xTxD
+        AÑADIDO POR MI (copiado)
+        """
+        # num_frames es la sliding window
+        num_overlap = int(num_frames * overlap)
+        max_len = x.shape[1]
+        embed = None
+        cur_iter = 0
+        for offset in range(0, max_len, num_frames - num_overlap):
+            cur_iter += 1
+            end_offset = min(x.shape[1], offset + num_frames)
+            frames = x[:, offset:end_offset]
+            if embed is None:
+                embed = self.inference(frames)
+            else:
+                embed += self.inference(frames)
+        return embed / cur_iter
+
+    def batch_compute_speaker_references(self, x, seq_lens, num_frames=160, overlap=0.5):
+        """
+        Generate embeddings for a batch of utterances
+        x: BxTxD
+        """
+        num_overlap = int(num_frames * overlap)
+        max_len = x.shape[1]
+        embed = None
+        num_iters = seq_lens / (num_frames - num_overlap)
+        cur_iter = 0
+        print(max_len, num_frames, num_overlap)
+        for offset in range(0, max_len, num_frames - num_overlap):
+            cur_iter += 1
+            end_offset = min(x.shape[1], offset + num_frames)
+            frames = x[:, offset:end_offset]
+            if embed is None:
+                embed = self.inference(frames)
+            else:
+                embed[cur_iter <= num_iters, :] += self.inference(
+                    frames[cur_iter <= num_iters, :, :]
+                )
+        return embed / num_iters
