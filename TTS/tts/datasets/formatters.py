0e12a16d5e03309db84761497833cd2d9bd5546b
Yonge Bai <97137323+YongeBai@users.noreply.github.com>
2023-06-17 23:13:24 -0400

    fixed Index OOB issue with ljspeech formatter

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -159,7 +159,7 @@ def ljspeech(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
         for line in ttf:
             cols = line.split("|")
             wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
-            text = cols[2]
+            text = cols[1]
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
     return items
 
