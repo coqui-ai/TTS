29a0e189b28ac9419772e80aaa259d86e7f4629a
David Martin Rius <0991592@gmail.com>
2024-03-05 22:38:59 +0100

    Revert "Merge remote-tracking branch 'subuday/matcha_tts' into dev"
    This reverts commit f6a23c1d8a153b40d5c06b81dbfda9b66d0fc4d6, reversing
    changes made to 275229a876e23c1fe3a7bf96d4712461e46f0af9.

--- TTS/tts/configs/matcha_tts.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-
-from TTS.tts.configs.shared_configs import BaseTTSConfig
-
-
-@dataclass
-class MatchaTTSConfig(BaseTTSConfig):
-    model: str = "matcha_tts"
-    num_chars: int = None
